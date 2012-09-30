32b532a57ee5dfe9f20ed94f0b71289d43b94025
Donna Whisnant <dewhisna@users.noreply.github.com>
2012-09-30 15:16:29 +0000

    This version has LAYOUT table gen, Word Dump, and Book Dump pretty well finished.  Getting setup to add BOOK table gen with rich text support.
    git-svn-id: file:///home/dewhisna/Documents/programs/Bible/svndump/purebiblesearch_150731/code_gitcvt/trunk@5 523e89ec-37fb-4b40-b97f-42a194c95123

--- programs/KJVCanOpener/kjvdatagen/kjvdatagen.c
+++ programs/KJVCanOpener/kjvdatagen/kjvdatagen.c
@@ -1,17 +1,28 @@
 //
 // KJVDataGen.c
 //
-//	Generates the Bk/Chp Keys and Data for the KJVCanOpener LAYOUT Table and
-//				Chp/Vrs Keys and Data for individual KJVCanOpener BOOK Tables
+//	Generates the Bk/Chp Keys and Data for the KJVCanOpener LAYOUT Table,
+//				Chp/Vrs Keys and Data for individual KJVCanOpener BOOK Tables,
+//				Word data for the WORDS Table,
+//				Word mapping indices for the MAP Table,
+//				and can create word and book dumps.
 //
-//	Reads a Sword OutPlain dump file with OSIS Key names and text from <stdin>
-//		and writes the LAYOUT table (sans footnotes) to <stdout> if "layout"
-//		is specified or the appropriate BOOK table if a book name is given.
+//	Reads a specialized (with '@' symbols) Sword OutPlain dump file with OSIS
+//		Key names and text from <stdin>.  Sends the following to <stdout>
+//		depending on the specified mode:
+//		layout :	Writes the LAYOUT table (sans footnotes)
+//		book :		Writes the BOOK table for the specified book name
+//		words :		Writes the WORDS table
+//		worddump :	Dumps all words, trimmed on individual lines
+//		bookdump :	Dumps the content of the specified book, one verse per line
 //
 // Input should be formatted so the OSIS reference is preceeded by "$$$" and
-//	the verse text is preceeded by "@" and ended with "@".  Newline \a characters
-//	within the verse text is treated as the Paragraph mark (as seen at the
-//	beginning of Gen 1:3).
+//	the verse text is preceeded by "@" and ended with "@".  Newline (\a) characters
+//	at the beginning of the verse text is treated as the Paragraph mark (pilcrow)
+//	as seen, for example, at the beginning of Gen 1:3.  Note that these only
+//	occur in the KJV text at the beginning of the verse, just after the verse #!
+//	Other newline (\a) characters are used to separate verse text from footnote
+//	text, etc.  Thus, parsing is halted if another newline is found.
 //
 // Input Format:
 //	$$$Gen.1.1
@@ -24,8 +35,36 @@
 //	@
 //	And God said, Let there be light: and there was light.@
 //
+// When outputing BOOK tables, input stream should include rich text as well:
+//
+//	$$$Gen.1.1
+//	@In the beginning God created the heaven and the earth.@
+//	@In the beginning God created the heaven and the earth.@
+//	$$$Gen.1.2
+//	@And the earth was without form, and void; and darkness was upon the face of the deep. And the Spirit of God moved upon the face of the waters.@
+//	@And the earth was without form, and void; and darkness <i>was</i> upon the face of the deep. And the Spirit of God moved upon the face of the waters.@
+//	$$$Gen.1.3
+//	@
+//	And God said, Let there be light: and there was light.@
+//	@And God said, Let there be light: and there was light.@
+//	$$$Gen.1.4
+//	@And God saw the light, that it was good: and God divided the light from the darkness.@
+//	@And God saw the light, that <i>it was</i> good: and God divided the light from the darkness.@
+//	$$$Gen.1.5
+//	@And God called the light Day, and the darkness he called Night. And the evening and the morning were the first day.@
+//	@And God called the light Day, and the darkness he called Night. And the evening and the morning were the first day.@
+//
 // Output Format (for LAYOUT):
-//	257,50,464,""
+//	{nBk|nChp},countVrs,countWrd,Footnotes$,BkAbbr$,Chp
+//
+// Example:
+//	257,31,797,,Gen,1
+//	258,25,632,,Gen,2
+//	259,24,695,,Gen,3
+//	260,26,632,,Gen,4
+//	261,32,504,,Gen,5
+//	262,22,579,,Gen,6
+//	263,24,584,,Gen,7
 //
 
 #include <stdlib.h>
@@ -39,6 +78,8 @@
 #endif
 
 #define NUM_BK 66
+#define NUM_BK_OT 39
+#define NUM_BK_NT 27
 
 const char *g_arrstrBkAbbr[NUM_BK] =
 		{	"Gen",
@@ -109,17 +150,47 @@ const char *g_arrstrBkAbbr[NUM_BK] =
 			"Rev"
 		};
 
-const char *g_strCharset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'-\x96\xE2\x80\x93\xE6\xC3\xA6\xB6#";		// Accept alphanumeric, -, ', #, æ, ¶, and Special x96 and E28093 UTF8 encoding of (-), and Special xE6 and C3A6 UTF8 encoding of (æ), (ugh!)
+const char *g_strCharset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'-";		// Accept: [alphanumeric, -, '], we'll handle UTF-8 conversion and translate those to ASCII as appropriate
+
+int fgetcUTF8(FILE *file)
+{
+	int j;
+	int c;
+	int c2;
+	int cResult;
+
+	c = fgetc(file);
+	if (c == EOF) return EOF;
+	if ((c & 0x0080) == 0) return c;		// Single bytes leave as-is
+	c = ((c & 0x007f) << 1);
+
+	cResult=0;
+	j=-1;					// Start with -1 because we've already shifted c by 1 bit
+	while (c & 0x0080) {
+		c2 = fgetc(file);
+		if (c2 == EOF) return EOF;			// Exit if we hit the end of the file prematurely (really, this is a character encoding error)
+		cResult = (cResult << 6) | (c2 & 0x3F);
+		j+=5;				// Shift left 5, 6 for the 6 bits we are adding, minus 1 because we're already shifted c
+		c = ((c & 0x007f) << 1);
+	}
+	cResult = cResult | (c << j);
+
+	return cResult;
+}
 
 int main(int argc, const char *argv[])
 {
-	char buff[10000];
+	char buffPlain[10000];
 	char word[50];
 	int countChp[NUM_BK];		// # of Chapters in books
 	int countVrs[NUM_BK];		// # of Verses in entire book for Book mode or # of Verses in current Chapter for Layout mode
 	int countWrd[NUM_BK];		// # of Words in entire book for Book mode or # of Words in current Chapter for Layout mode
-	BOOL bDoingLayout = FALSE;
-	BOOL bDoingWordDump = FALSE;
+	BOOL bDoingLayout = FALSE;		// TRUE if outputing LAYOUT table
+	BOOL bDoingBook = FALSE;		// TRUE if outputing BOOK table (nBkNdx = book to output)
+	BOOL bDoingWords = FALSE;		// TRUE if outputing WORDS table
+	BOOL bDoingWordDump = FALSE;	// TRUE if dumping words used (nBkNdx = book to output or 0 for all)
+	BOOL bDoingBookDump = FALSE;	// TRUE if dumping book content (nBkNdx = book to output or 0 for all)
+	BOOL bNeedUsage = FALSE;		// TRUE if user needs usage info
 	int nBkNdx = 0;		// Index of Book to Output (Book Mode)
 	int nCurBk = 0;		// Current book output (Layout Mode)
 	int nCurChp = 0;	// Current chapter output (Layout Mode)
@@ -128,39 +199,58 @@ int main(int argc, const char *argv[])
 	int nVrs;			// Verse counter
 	int nWrd;			// Word counter
 	int ndx;			// Index into buffer;
+	BOOL bIsPilcrow;	// Set to TRUE if the current verse is a Pilcrow (¶) start point, else is FALSE
 	int i;
 	int c;				// Character from input file
 	char *pTemp;
 	char *pTemp2;
 
 	if (argc < 2) {
-		fprintf(stderr, "Usage:  KJVDataGen <mode>\n\n");
-		fprintf(stderr, "  Where <mode> is either a Bible book name or one of:\n\n");
-		fprintf(stderr, "      \"layout\" -- Dump LAYOUT table data\n");
-		fprintf(stderr, "      \"words\" -- Dump out all words\n\n");
-		fprintf(stderr, "  I/O is from/to stdin/stdout.  Input should be special\n");
-		fprintf(stderr, "  Sword outplain dump file.\n");
-		return -1;
+		bNeedUsage = TRUE;
+	} else if (stricmp(argv[1], "layout") == 0) {
+		bDoingLayout = TRUE;
+	} else if (stricmp(argv[1], "book") == 0) {
+		bDoingBook = TRUE;
+		if (argc < 3) bNeedUsage = TRUE;		// Must have book name for this one
+	} else if (stricmp(argv[1], "words") == 0) {
+		bDoingWords = TRUE;
+	} else if (stricmp(argv[1], "worddump") == 0) {
+		bDoingWordDump = TRUE;
+	} else if (stricmp(argv[1], "bookdump") == 0) {
+		bDoingBookDump = TRUE;
+	} else {
+		bNeedUsage = TRUE;
 	}
 
-	if (stricmp(argv[1], "layout") == 0) {
-		bDoingLayout = TRUE;
-	} else if (stricmp(argv[1], "words") == 0) {
-		bDoingWordDump = TRUE;
-	} else {
+	if (argc > 2) {
 		for (nBk = 0; nBk < NUM_BK; ++nBk) {
-			if (stricmp(g_arrstrBkAbbr[nBk], argv[1]) == 0) {
+			if (stricmp(g_arrstrBkAbbr[nBk], argv[2]) == 0) {
 				nBkNdx = nBk + 1;
 				break;
 			}
 		}
 		if (nBkNdx == 0) {
-			fprintf(stderr, "Invalid mode or book name.  Valid books are:\n\n");
+			fprintf(stderr, "Invalid book name.  Valid books are:\n\n");
 			for (nBk = 0; nBk < NUM_BK; ++nBk) {
 				fprintf(stderr, "    %s\n", g_arrstrBkAbbr[nBk]);
 			}
 			return -1;
 		}
+	}
+	// By here, nBkNum = Book number to output, or 0 for all books on modes that allow it
+
+	if (bNeedUsage) {
+		fprintf(stderr, "Usage:  KJVDataGen <mode> [<bookname>]\n\n");
+		fprintf(stderr, "  Where <mode> is one of:\n\n");
+		fprintf(stderr, "      layout   -- Dump LAYOUT table data (book name not used)\n");
+		fprintf(stderr, "      book     -- Dump BOOK table data for the specified book\n");
+		fprintf(stderr, "      words    -- Dump WORDS table data (book name not used)\n");
+		fprintf(stderr, "      worddump -- Dumps all words (book name optional)\n");
+		fprintf(stderr, "      bookdump -- Dumps the content of specified book (Verse per line)\n");
+		fprintf(stderr, "\n\n");
+		fprintf(stderr, "  I/O is from/to stdin/stdout.  Input should be specialized\n");
+		fprintf(stderr, "  Sword outplain dump file.\n");
+		return -1;
 	}
 
 	// Initialize total counts for the book
@@ -179,20 +269,20 @@ int main(int argc, const char *argv[])
 	while (!feof(stdin)) {
 		for (i=0; i<3; ++i) {		// looking for 3 '$' symbols
 			do {
-				c = fgetc(stdin);
+				c = fgetcUTF8(stdin);
 			} while ((c != '$') && (c != EOF));
 			if (c == EOF) break;
 		}
 		if (c == EOF) break;
 
-		fgets(buff, sizeof(buff), stdin);
+		fgets(buffPlain, sizeof(buffPlain), stdin);	// Note: The reference shouldn't be UTF-8, so we can just use fgets
 		nBk = nChp = nVrs = 0;
-		pTemp = strchr(buff, '.');			// Find book name
+		pTemp = strchr(buffPlain, '.');			// Find book name
 		if (pTemp) {
 			*pTemp = 0;
 			pTemp++;
 			for (nBk = 0; nBk < NUM_BK; ++nBk) {
-				if (stricmp(g_arrstrBkAbbr[nBk], buff) == 0) break;
+				if (stricmp(g_arrstrBkAbbr[nBk], buffPlain) == 0) break;
 			}
 			if (nBk == NUM_BK) {
 				nBk = 0;			// Set to 0 if we didn't find it
@@ -211,7 +301,7 @@ int main(int argc, const char *argv[])
 			}
 		}
 		if ((nBk == 0) || (nChp == 0) || (nVrs == 0)) {
-			fprintf(stderr, "Unable to parse: %s\n\n", buff);
+			fprintf(stderr, "Unable to parse: %s\n\n", buffPlain);
 			continue;
 		}
 
@@ -219,20 +309,49 @@ int main(int argc, const char *argv[])
 		if (nVrs > countVrs[nBk-1]) countVrs[nBk-1] = nVrs;
 
 		do {							// Find "@" marker
-			c = fgetc(stdin);
+			c = fgetcUTF8(stdin);
 		} while ((c != '@') && (c != EOF));
 		if (c == EOF) break;
 
-		// Read and fill buff with verse text up to final "@" marker:
+		// Read and fill buffPlain with verse text up to final "@" marker:
+		bIsPilcrow = FALSE;
 		ndx = 0;
 		do {
-			c = fgetc(stdin);
+			c = fgetcUTF8(stdin);
 			if ((c != '@') && (c != EOF)) {
-				buff[ndx] = c;
+				if (c == 0x0a) {
+					if (ndx > 0) break;			// A newline after text means footnote marker!  So skip the rest!!
+					bIsPilcrow = TRUE;			// A newline at the start denotes a Pilcrow (¶) mark
+					continue;					// We set the flag, but we'll exclude writing the newline to the output
+				}
+				if (c == 0xb6) {				// Translate Pilcrow
+					bIsPilcrow = TRUE;
+					continue;
+				}
+				if (c == 0x2013) c = '-';		// Translate "en dash" to (-)
+				if (c == 0x2015) {				// Translate "Horizontal Bar" to (--) TODO : Figure out what to do with this one!
+					buffPlain[ndx] = '-';
+					ndx++;
+					c = '-';
+				}
+				if (c == 0xe6) {				// Translate "æ" to "ae":
+					buffPlain[ndx] = 'a';
+					ndx++;
+					c = 'e';
+				}
+				if (c == 0xc6) {				// Translate "Æ" to "Ae":
+					buffPlain[ndx] = 'A';
+					ndx++;
+					c = 'e';
+				}
+				if (c > 127) {
+					fprintf(stderr, "Unexpected UTF-8 symbol (%04lx) at position %d [0x%04lx] in file: %s %0d.%0d [%0d]\n", c, ftell(stdin), ftell(stdin), g_arrstrBkAbbr[nBk-1], nChp, nVrs, ndx);
+				}
+				buffPlain[ndx] = c;
 				ndx++;
 			}
-		} while ((!feof(stdin)) && (ndx < sizeof(buff)-1) && (c != '@'));
-		buff[ndx] = 0;
+		} while ((!feof(stdin)) && (ndx < sizeof(buffPlain)-1) && (c != '@'));
+		buffPlain[ndx] = 0;
 
 		if (bDoingLayout) {
 			// Output the current one we were processing just before transitioning:
@@ -245,26 +364,27 @@ int main(int argc, const char *argv[])
 			}
 		}
 
+		if ((bDoingBookDump) && ((nBkNdx == 0) || (nBk == nBkNdx))) {
+			fprintf(stdout, "%s\n", buffPlain);
+		}
+
 		// Do this after the printing because either we are on the same
 		//		chp/bk in which case we didn't print the output line above
 		//		and it doesn't matter on the order, or else this word count
 		//		is for the next chp or bk and should count toward it instead
 		//		after we've written the output for our current Chp/Bk.
-//fprintf(stdout, ",,,,");
 		nWrd = 0;
-		pTemp = strpbrk(buff, g_strCharset);
+		pTemp = strpbrk(buffPlain, g_strCharset);
 		while (pTemp) {								// pTemp = start of word
 			nWrd++;
 			pTemp2 = pTemp+1;
-			while (strchr(g_strCharset, *pTemp2) != NULL) ++pTemp2;		// pTemp2 = End of word (TODO : for future Word dump)
-			strncpy(word, pTemp, pTemp2-pTemp);
+			while ((*pTemp2 != 0) && (strchr(g_strCharset, *pTemp2) != NULL)) ++pTemp2;
+			memcpy(word, pTemp, pTemp2-pTemp);
 			word[pTemp2-pTemp] = 0;
-			if (bDoingWordDump) fprintf(stdout, "%s\n", word);
-//fprintf(stdout, "%s;", word);
+			if ((bDoingWordDump) && ((nBkNdx == 0) || (nBk == nBkNdx))) fprintf(stdout, "%s\n", word);
 			pTemp = strpbrk(pTemp2, g_strCharset);
 		}		// Here, nWrd = Number of words in this verse
 		countWrd[nBk-1] += nWrd;			// Add in the number of words we found
-//fprintf(stdout, ",%d\n", nWrd);
 	}
 
 	if (bDoingLayout) {
