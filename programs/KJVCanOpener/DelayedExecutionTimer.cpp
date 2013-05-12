ba1580468227c7e98fa749d53d30ca89ef0a2526
Donna Whisnant <dewhisna@users.noreply.github.com>
2013-05-12 05:20:35 +0000

    Added DelayedExecutionTimer from https://qt-project.org/wiki/Delay_action_to_wait_for_user_interaction to use with delaying execution of the spinboxes on KJVBrowser and perhaps for the QCompleter on KJVSearchPhraseEdit.
    git-svn-id: file:///home/dewhisna/Documents/programs/Bible/svndump/purebiblesearch_150731/code_gitcvt/trunk@339 523e89ec-37fb-4b40-b97f-42a194c95123

---
+++ programs/KJVCanOpener/DelayedExecutionTimer.cpp
@@ -0,0 +1,64 @@
+/*
+Copyright (c) 2011, Andre Somers
+All rights reserved.
+
+File licence not repeated here for space reasons. See file "DelayedExecutionTimer.h" for details of licence.
+*/
+
+#include "DelayedExecutionTimer.h"
+#include <QTimer>
+#include <QStringBuilder>
+
+DelayedExecutionTimer::DelayedExecutionTimer(int maximumDelay, int minimumDelay, QObject* parent):
+	QObject(parent),
+	m_minimumDelay(minimumDelay),
+	m_maximumDelay(maximumDelay),
+	m_minimumTimer(new QTimer(this)),
+	m_maximumTimer(new QTimer(this)),
+	m_lastInt(0)
+{
+	connect(m_minimumTimer, SIGNAL(timeout()), SLOT(timeout()));
+	connect(m_maximumTimer, SIGNAL(timeout()), SLOT(timeout()));
+}
+
+DelayedExecutionTimer::DelayedExecutionTimer(QObject* parent):
+	QObject(parent),
+	m_minimumDelay(250),
+	m_maximumDelay(1000),
+	m_minimumTimer(new QTimer(this)),
+	m_maximumTimer(new QTimer(this)),
+	m_lastInt(0)
+{
+	connect(m_minimumTimer, SIGNAL(timeout()), SLOT(timeout()));
+	connect(m_maximumTimer, SIGNAL(timeout()), SLOT(timeout()));
+}
+
+void DelayedExecutionTimer::timeout()
+{
+	m_minimumTimer->stop();
+	m_maximumTimer->stop();
+	emit triggered();
+	emit triggered(m_prefix % m_lastString % m_postfix);
+	emit triggered(m_lastInt);
+}
+
+void DelayedExecutionTimer::trigger()
+{
+	if (!m_maximumTimer->isActive()) {
+		m_maximumTimer->start(m_maximumDelay);
+	}
+	m_minimumTimer->stop();
+	m_minimumTimer->start(m_minimumDelay);
+}
+
+void DelayedExecutionTimer::trigger(QString string)
+{
+	m_lastString = string;
+	trigger();
+}
+
+void DelayedExecutionTimer::trigger(int i)
+{
+	m_lastInt = i;
+	trigger();
+}
