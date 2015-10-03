eacb1bca9f8cdf7141ef0afab6e982d3b7456703
Donna Whisnant <dewhisna@users.noreply.github.com>
2015-10-03 02:15:47 -0500

    Added Bible Audio Support for Dan Wagner, Stephen Johnston, and Christopher Glyn recordings in addition to the two Faith Comes By Hearing recordings. Changed the default Bible Audio to be "no selection" to avoid triggering streaming audio from the server if the user doesn't want to use it. Removed the annoying tooltip text for the Bible Audio Selection and replaced it by the "no selection" selection described above. Added an "Auto Play Next Chapter" function for Bible Audio to trigger auto playing and advancing to the next chapter when current chapter finishes. Added user-selectable variable playback rate or speed for the Bible Audio.

--- programs/qwebchannel/webChannelBibleAudio.cpp
+++ programs/qwebchannel/webChannelBibleAudio.cpp
@@ -34,7 +34,12 @@
 namespace {
 
 #define NUM_BK 66
+#define NUMBERS_BOOK_NUM 04
 #define PSALMS_BOOK_NUM 19
+#define PHILEMON_BOOK_NUM 57
+#define _2JOHN_BOOK_NUM 63
+#define _3JOHN_BOOK_NUM 64
+#define JUDE_BOOK_NUM 65
 
 	// Book names as they appear on the FCBH recording files:
 	const QString g_arrconstrFCBHBooks[NUM_BK] = {
@@ -109,6 +114,230 @@ namespace {
 	const QString g_constrFCBHNonDramaURL = "http://audios.dewtronics.com/KingJamesBible/ON_NonDrama/%1";
 	const QString g_constrFCBHDramaURL = "http://audios.dewtronics.com/KingJamesBible/ON_Drama/%1";
 
+	// ------------------------------------------------------------------------
+
+	// Book names as they appear on the DanWagner recording files:
+	const QString g_arrconstrDWBooks[NUM_BK] = {
+		"Genesis",
+		"Exodus",
+		"Leviticus",
+		"Numbers",
+		"Deuteronomy",
+		"Joshua",
+		"Judges",
+		"Ruth",
+		"1Samuel",
+		"2Samuel",
+		"1Kings",
+		"2Kings",
+		"1Chronicles",
+		"2Chronicles",
+		"Ezra",
+		"Nehemiah",
+		"Esther",
+		"Job",
+		"Psalms",
+		"Proverbs",
+		"Ecclesiastes",
+		"Song_of_Solomon",
+		"Isaiah",
+		"Jeremiah",
+		"Lamentations",
+		"Ezekiel",
+		"Daniel",
+		"Hosea",
+		"Joel",
+		"Amos",
+		"Obadiah",
+		"Jonah",
+		"Micah",
+		"Nahum",
+		"Habakkuk",
+		"Zephaniah",
+		"Haggai",
+		"Zechariah",
+		"Malachi",
+		"Matthew",
+		"Mark",
+		"Luke",
+		"John",
+		"Acts",
+		"Romans",
+		"1Corinthians",
+		"2Corinthians",
+		"Galatians",
+		"Ephesians",
+		"Philippians",
+		"Colossians",
+		"1Thessalonians",
+		"2Thessalonians",
+		"1Timothy",
+		"2Timothy",
+		"Titus",
+		"Philemon",
+		"Hebrews",
+		"James",
+		"1Peter",
+		"2Peter",
+		"1John",
+		"2John",
+		"3John",
+		"Jude",
+		"Revelation"
+	};
+
+	const QString g_constrDWURL = "http://audios.dewtronics.com/KingJamesBible/DanWagner/%1";
+
+	// ------------------------------------------------------------------------
+
+	// Book names as they appear on the StephenJohnston recording files:
+	const QString g_arrconstrSJBooks[NUM_BK] = {
+		"Gen",
+		"Exo",
+		"Lev",
+		"Num",
+		"Deu",
+		"Jos",
+		"Jdg",
+		"Rth",
+		"1Sa",
+		"2Sa",
+		"1Ki",
+		"2Ki",
+		"1Ch",
+		"2Ch",
+		"Ezr",
+		"Neh",
+		"Est",
+		"Job",
+		"Psa",
+		"Pro",
+		"Ecc",
+		"Son",
+		"Isa",
+		"Jer",
+		"Lam",
+		"Eze",
+		"Dan",
+		"Hos",
+		"Joe",
+		"Amo",
+		"Oba",
+		"Jon",
+		"Mic",
+		"Nah",
+		"Hab",
+		"Zep",
+		"Hag",
+		"Zec",
+		"Mal",
+		"Mat",
+		"Mar",
+		"Luk",
+		"Joh",
+		"Act",
+		"Rom",
+		"1Co",
+		"2Co",
+		"Gal",
+		"Eph",
+		"Php",
+		"Col",
+		"1Th",
+		"2Th",
+		"1Ti",
+		"2Ti",
+		"Tts",
+		"Phm",
+		"Heb",
+		"Jam",
+		"1Pe",
+		"2Pe",
+		"1Jo",
+		"2Jo",
+		"3Jo",
+		"Jde",
+		"Rev"
+	};
+
+	const QString g_constrSJURL = "http://audios.dewtronics.com/KingJamesBible/StephenJohnston/%1";
+
+	// ------------------------------------------------------------------------
+
+	// Book names as they appear on the ChristopherGlyn recording files:
+	const QString g_arrconstrCGBooks[NUM_BK] = {
+		"Genesis",
+		"Exodus",
+		"Leviticus",
+		"Numbers",
+		"Deuteronomy",
+		"Joshua",
+		"Judges",
+		"Ruth",
+		"1-Samuel",
+		"2-Samuel",
+		"1-Kings",
+		"2-Kings",
+		"1-Chronicles",
+		"2-Chronicles",
+		"Ezra",
+		"Nehemiah",
+		"Esther",
+		"Job",
+		"Psalm",
+		"Proverbs",
+		"Ecclesiastes",
+		"Song_of_Solomon",
+		"Isaiah",
+		"Jeremiah",
+		"Lamentations",
+		"Ezekiel",
+		"Daniel",
+		"Hosea",
+		"Joel",
+		"Amos",
+		"Obadiah",
+		"Jonah",
+		"Micah",
+		"Nahum",
+		"Habakkuk",
+		"Zephaniah",
+		"Haggai",
+		"Zechariah",
+		"Malachi",
+		"Matthew",
+		"Mark",
+		"Luke",
+		"John",
+		"Acts",
+		"Romans",
+		"1Corinthians",
+		"2Corinthians",
+		"Galatians",
+		"Ephesians",
+		"Philippians",
+		"Colossians",
+		"1Thessalonians",
+		"2Thessalonians",
+		"1Timothy",
+		"2Timothy",
+		"Titus",
+		"Philemon",
+		"Hebrews",
+		"James",
+		"1Peter",
+		"2Peter",
+		"1John",
+		"2John",
+		"3John",
+		"Jude",
+		"Revelation"
+	};
+
+	const QString g_constrCGURL = "http://audios.dewtronics.com/KingJamesBible/ChristopherGlyn/%1";
+
+	// ------------------------------------------------------------------------
+
 }	// Namespace
 
 // ============================================================================
@@ -150,34 +379,79 @@ QString CWebChannelBibleAudio::urlsForChapterAudio(const CBibleDatabasePtr pBibl
 		int nBk = CRefCountCalc(pBibleDatabase.data(), CRefCountCalc::RTE_BOOK, ndxDecolophonated).ofBible().first;
 		int nChp = CRefCountCalc(pBibleDatabase.data(), CRefCountCalc::RTE_CHAPTER, ndxDecolophonated).ofBook().first;
 
-		bool bFCBHValid = (((nTst > 0) && (nTst <= 2)) && (nBkTst != 0) && ((nBk > 0) && (nBk <= NUM_BK)));
+		bool bKJVValid = (((nTst > 0) && (nTst <= 2)) && (nBkTst != 0) && ((nBk > 0) && (nBk <= NUM_BK)));
 		if ((pBibleDatabase->compatibilityUUID().compare(bibleDescriptor(BDE_KJV).m_strUUID, Qt::CaseInsensitive) != 0) &&
 			(pBibleDatabase->compatibilityUUID().compare(bibleDescriptor(BDE_KJVPCE).m_strUUID, Qt::CaseInsensitive) != 0) &&
 			(pBibleDatabase->compatibilityUUID().compare(bibleDescriptor(BDE_KJVA).m_strUUID, Qt::CaseInsensitive) != 0) &&
 			(pBibleDatabase->compatibilityUUID().compare(bibleDescriptor(BDE_KJV1611).m_strUUID, Qt::CaseInsensitive) != 0) &&
 			(pBibleDatabase->compatibilityUUID().compare(bibleDescriptor(BDE_KJV1611A).m_strUUID, Qt::CaseInsensitive) != 0))
-			bFCBHValid = false;
+			bKJVValid = false;
 
-		if (bFCBHValid) {
+		if (bKJVValid) {
+			// Faith Comes By Hearing:
 			QString strFCBHBookName = QString("%1").arg(g_arrconstrFCBHBooks[nBk-1], -12, QChar('_'));
-			QString strBkTst = QString("%1%2").arg((nTst == 1) ? "A" : "B").arg(nBkTst, 2, 10, QChar('0'));
-			QString strChp = ((nBk == PSALMS_BOOK_NUM) ?
+			QString strFCBHBkTst = QString("%1%2").arg((nTst == 1) ? "A" : "B").arg(nBkTst, 2, 10, QChar('0'));
+			QString strFCBHChp = ((nBk == PSALMS_BOOK_NUM) ?
 								QString("%1").arg(nChp, 3, 10, QChar('0')) :
 								QString("_%1").arg(nChp, 2, 10, QChar('0')));
-			QString strFCBHNonDrama = QString("%1__%2_%3ENGKJVC1DA.mp3").arg(strBkTst).arg(strChp).arg(strFCBHBookName);
-			QString strFCBHDrama = QString("%1__%2_%3ENGKJVC2DA.mp3").arg(strBkTst).arg(strChp).arg(strFCBHBookName);
+			QString strFCBHNonDrama = QString("%1__%2_%3ENGKJVC1DA.mp3").arg(strFCBHBkTst).arg(strFCBHChp).arg(strFCBHBookName);
+			QString strFCBHDrama = QString("%1__%2_%3ENGKJVC2DA.mp3").arg(strFCBHBkTst).arg(strFCBHChp).arg(strFCBHBookName);
 			if (flagsBAS & BAS_FCBH_NONDRAMA) {
 				QJsonObject objBibleAudio;
-				objBibleAudio["name"] = "Non-Drama";
+				objBibleAudio["name"] = "FaithComesByHearing Non-Drama";
 				objBibleAudio["url"] = QString(g_constrFCBHNonDramaURL).arg(strFCBHNonDrama);
 				arrBibleAudioList.append(objBibleAudio);
 			}
 			if (flagsBAS & BAS_FCBH_DRAMA) {
 				QJsonObject objBibleAudio;
-				objBibleAudio["name"] = "Drama";
+				objBibleAudio["name"] = "FaithComesByHearing Drama";
 				objBibleAudio["url"] = QString(g_constrFCBHDramaURL).arg(strFCBHDrama);
 				arrBibleAudioList.append(objBibleAudio);
 			}
+
+			// Dan Wagner:
+			QString strDWBookFolder = QString("%1_%2").arg(nBk, 2, 10, QChar('0')).arg(g_arrconstrDWBooks[nBk-1]);
+			QString strDWBkChp;
+			if (nBk == NUMBERS_BOOK_NUM) {
+				strDWBkChp = QString("%1%2").arg(g_arrconstrDWBooks[nBk-1]).arg(nChp, 2, 10, QChar('0'));
+			} else if ((nBk == PHILEMON_BOOK_NUM) ||
+					   (nBk == _2JOHN_BOOK_NUM) ||
+					   (nBk == _3JOHN_BOOK_NUM) ||
+					   (nBk == JUDE_BOOK_NUM)) {
+				strDWBkChp = g_arrconstrDWBooks[nBk-1];
+			} else {
+				strDWBkChp = QString("%1%2").arg(g_arrconstrDWBooks[nBk-1]).arg(nChp, 3, 10, QChar('0'));
+			}
+			QString strDanWagner = QString("%1/%2.mp3").arg(strDWBookFolder).arg(strDWBkChp);
+			if (flagsBAS & BAS_DAN_WAGNER) {
+				QJsonObject objBibleAudio;
+				objBibleAudio["name"] = "Dan Wagner";
+				objBibleAudio["url"] = QString(g_constrDWURL).arg(strDanWagner);
+				arrBibleAudioList.append(objBibleAudio);
+			}
+
+			// Stephen Johnston:
+			QString strSJBkChp = QString("%1%2%3.mp3").arg(nBk, 2, 10, QChar('0')).arg(g_arrconstrSJBooks[nBk-1]).arg(nChp, 3, 10, QChar('0'));
+			if (flagsBAS & BAS_STEPHEN_JOHNSTON) {
+				QJsonObject objBibleAudio;
+				objBibleAudio["name"] = "Stephen Johnston";
+				objBibleAudio["url"] = QString(g_constrSJURL).arg(strSJBkChp);
+				arrBibleAudioList.append(objBibleAudio);
+			}
+
+			// Christophen Glyn:
+			QString strCGTstFolder = QString("%1").arg((nTst == 1) ? "ot" : "nt");
+			QString strCGBkChp = QString("%1_%2_%3").arg(nBkTst, 2, 10, QChar('0'))
+													.arg(g_arrconstrCGBooks[nBk-1])
+													.arg(nChp, (nBk == PSALMS_BOOK_NUM) ? 3 : 2, 10, QChar('0'));
+			QString strChristopherGlyn = QString("%1/%2.mp3").arg(strCGTstFolder).arg(strCGBkChp);
+			if (flagsBAS & BAS_CHRISTOPHER_GLYN) {
+				QJsonObject objBibleAudio;
+				objBibleAudio["name"] = "Christopher Glyn";
+				objBibleAudio["url"] = QString(g_constrCGURL).arg(strChristopherGlyn);
+				arrBibleAudioList.append(objBibleAudio);
+			}
+
 		}
 	}
 
--- programs/qwebchannel/webChannelBibleAudio.h
+++ programs/qwebchannel/webChannelBibleAudio.h
@@ -21,6 +21,62 @@
 **
 ****************************************************************************/
 
+/****************************************************************************
+
+  -------------------------------------
+  Audio Bible Texts Supported by KJPBS:
+  -------------------------------------
+
+  Faith Comes By Hearing : https://www.faithcomesbyhearing.com/
+	2421 Aztec Rd NE Albuquerque, NM 87107
+	800.545.6552 | 505.881.3321
+	info@faithcomesbyhearing.com
+	http://www.bible.is/terms
+	Free
+
+  Alexander Scourby : http://scourby.com/
+	info@litchfieldltd.com
+	US Phone Contact 813-997-2470
+	UK Phone Contact +44-28 9568-0022
+	http://scourby.com/terms_page.php?q=2
+	NOT Free
+
+  Dan Wagner : http://www.mp3bible.ca/
+	This audio Bible is produced by Audio Scriptures International.
+	Below you will find the transcript of their copyright audio tag:
+	"Thank you for listening to this recording of the King James Bible
+	produced by Audio Scriptures International. Audio Scriptures
+	International believes it is more important to spread the Word of
+	God than to make a large profit. Please feel free to make copies of
+	these recordings and distribute them freely so long as you adhere
+	to the following guidelines:
+
+	1. You may not charge any more than the cost to duplicate and send them.
+	2. You must make a complete copy of the recording, including this message.
+
+	For commercial and fund raising distribution, please contact Audio
+	Scriptures International for additional information."
+	Listen to copyright notice : http://www.mp3bible.ca/copyright/Audio_Scriptures_International_Copyright_Info.mp3
+
+  Stephen Johnston : http://www.audiotreasure.com/indexKJV.htm
+	The King James Version Bible
+	(c)2002 Firefighters for Christ / Narrated by Stephen Johnston/ Used by permission
+
+  Christopher Glyn : http://www.audiobiblekjv.com/
+	RadioActive Productions
+	Telephone: U.S.: 281-941-5678
+	Address: P.O. Box 126, La Porte, Texas 77572
+	Free
+
+  Jon Sherberg and James Earl Jones : http://www.amazon.com/gp/product/1600775845
+	Publisher: Topics Entertainment (October 20, 2009)
+	Language: English
+	ISBN-10: 1600775845
+	ISBN-13: 978-1600775840
+	NOT Free
+
+****************************************************************************/
+
 #ifndef WEBCHANNEL_BIBLE_AUDIO_H
 #define WEBCHANNEL_BIBLE_AUDIO_H
 
@@ -37,8 +93,14 @@ public:
 	// Bit-Flag list of sources to include
 	enum BibleAudioSources {
 		BAS_NONE = 0,
-		BAS_FCBH_NONDRAMA = 1,				// Faith Comes By Hearing Non-Drama Version
-		BAS_FCBH_DRAMA = 2,					// Faith Comes By Hearing Drama Version
+		BAS_FCBH_NONDRAMA = 0x1,			// Faith Comes By Hearing Non-Drama Version
+		BAS_FCBH_DRAMA = 0x2,				// Faith Comes By Hearing Drama Version
+		BAS_SCOURBY_NONDRAMA = 0x4,			// Alexandander Scourby Non-Drama Version
+		BAS_SCOURBY_DRAMA = 0x8,			// Alexandander Scourby Drama Version
+		BAS_DAN_WAGNER = 0x10,				// Dan Wagner (Retired California Minister)
+		BAS_STEPHEN_JOHNSTON = 0x20,		// Stephen Johnston Version
+		BAS_CHRISTOPHER_GLYN = 0x40,		// Christopher Glyn Version
+		BAS_SHERBERG_JONES = 0x80,			// Jon Sherberg and James Earl Jones Version
 		BAS_ALL = 0xFFFFFFFF
 	};
 	Q_DECLARE_FLAGS(BibleAudioSourcesFlags, BibleAudioSources)
