6564076e9760a698ee29933dc065b2cc2c312d88
Donna Whisnant <dewhisna@users.noreply.github.com>
2015-10-04 23:46:56 -0500

    Added Bible Descriptor for RVG2010-20150120 text.

--- programs/KJVCanOpener/dbDescriptors.cpp
+++ programs/KJVCanOpener/dbDescriptors.cpp
@@ -63,6 +63,7 @@ namespace {
 	const char *constUUID_RV1865sbv20140622 =	"919DAB50-1151-11E4-9191-0800200C9A66";
 	const char *constUUID_RVG2010_20140705 =	"92EF65E0-56F0-11E4-8ED6-0800200C9A66";
 	const char *constUUID_KJF2015 =				"36708290-9BB0-11E4-BD06-0800200C9A66";
+	const char *constUUID_RVG2010_20150120 =	"925744F0-6B1B-11E5-A837-0800200C9A66";
 
 	// Use the RVG2010-201401026 UUID for the highlighter tag for RVG2010 in general.  Even
 	//		though they technically aren't compatible, since we've already released the
@@ -105,7 +106,9 @@ namespace {
 		// RVG2010-20140705:
 		{ BibleTypeOptionsFlags(defaultBibleTypeFlags), "RVG2010-20140705", "es", QString::fromUtf8("Reina-Valera Gómez 2010"), QString::fromUtf8("Reina-Valera Gómez 2010 Version (20140705)"), constUUID_RVG2010_20140705, "bbl-rvg2010-20140705.s3db", "bbl-rvg2010-20140705.ccdb", constUUID_RVG2010 },
 		// KJF2015 (Nadine's Work-In-Progress:
-		{ BibleTypeOptionsFlags(defaultBibleTypeFlags), "KJF2015", "fr", QString::fromUtf8("King James Française 2015 Work-In-Progress"), QString::fromUtf8("la Bible King James Française, édition 2015 Work-In-Progress"), constUUID_KJF2015, "bbl-kjf2015-workinprogress.s3db", "bbl-kjf2015-workinprogress.ccdb", constUUID_KJF2015 }
+		{ BibleTypeOptionsFlags(defaultBibleTypeFlags), "KJF2015", "fr", QString::fromUtf8("King James Française 2015 Work-In-Progress"), QString::fromUtf8("la Bible King James Française, édition 2015 Work-In-Progress"), constUUID_KJF2015, "bbl-kjf2015-workinprogress.s3db", "bbl-kjf2015-workinprogress.ccdb", constUUID_KJF2015 },
+		// RVG2010-20150120:
+		{ BibleTypeOptionsFlags(defaultBibleTypeFlags), "RVG2010-20150120", "es", QString::fromUtf8("Reina-Valera Gómez 2010"), QString::fromUtf8("Reina-Valera Gómez 2010 Version (20150120)"), constUUID_RVG2010_20150120, "bbl-rvg2010-20150120.s3db", "bbl-rvg2010-20150120.ccdb", constUUID_RVG2010 }
 	};
 
 	//////////////////////////////////////////////////////////////////////
