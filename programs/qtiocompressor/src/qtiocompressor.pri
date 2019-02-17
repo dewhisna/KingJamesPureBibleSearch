dewhisna/KingJamesPureBibleSearch

commit 6854e53b949ad2e2bf85caaa677ab43eb77a65d3

Update qtiocompressor to either include system-zlib or compile internal zlib. Newer prebuilt Qt packages don't include qt-zlib and don't implicitly link system-zlib.  So without this, it won't link.

programs/qtiocompressor/src/qtiocompressor.pri
@@ -31,5 +31,13 @@ android|ios {
 macx {
 	greaterThan(QT_MAJOR_VERSION,4):include($$[QT_INSTALL_PREFIX]/src/3rdparty/zlib.pri)
 }
+!android:!ios:!macx {
+	!contains(QT_CONFIG, system-zlib) {
+		include($$[QT_INSTALL_PREFIX]/src/3rdparty/zlib.pri)
+	} else {
+		if(unix|win32-g++*):LIBS += -lz
+		else:               LIBS += zdll.lib
+	}
+}
 
 }

commit 36c5f3d15a0401e4132e3e4d4065d777b1ecf5cf

Fix qtiocompressor zlib include for the Windows target. This was broken in 6854e53b949ad2e2bf85caaa677ab43eb77a65d3 when fixing it for Linux. On Windows, that change was causing duplicate symbols in QtBase for the zlib components when Qt was built with zlib support. This change only builds zlib if Qt doesn't already expose it.

programs/qtiocompressor/src/qtiocompressor.pri
@@ -8,7 +8,11 @@ DEPENDPATH += $$PWD
 # We need to find zlib.h. If it's not in the system includes, this will
 # find the one includes with Qt, assuming a developer-style "in-place" install.
 # Otherwise, edit this to point at the directory where zlib.h resides.
-INCLUDEPATH += $$[QT_INSTALL_PREFIX]/src/3rdparty/zlib
+exists($$[QT_INSTALL_PREFIX]/include/QtZlib/zlib.h) {
+	INCLUDEPATH += $$[QT_INSTALL_PREFIX]/include/QtZlib
+} else {
+	INCLUDEPATH += $$[QT_INSTALL_PREFIX]/src/3rdparty/zlib
+}
 
 qtiocompressor-uselib:!qtiocompressor-buildlib {
     LIBS += -L$$QTIOCOMPRESSOR_LIBDIR -l$$QTIOCOMPRESSOR_LIBNAME
@@ -33,7 +37,9 @@ macx {
 }
 !android:!ios:!macx {
 	!contains(QT_CONFIG, system-zlib) {
-		include($$[QT_INSTALL_PREFIX]/src/3rdparty/zlib.pri)
+		!contains(QT_CONFIG, zlib) {
+			include($$[QT_INSTALL_PREFIX]/src/3rdparty/zlib.pri)
+		}
 	} else {
 		if(unix|win32-g++*):LIBS += -lz
 		else:               LIBS += zdll.lib