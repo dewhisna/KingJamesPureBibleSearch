dewhisna/KingJamesPureBibleSearch

commit 69852cc11df731d817e80b88944a0e04c3e1563f

Cleanup command-line vs. GUI includes so all apps build on Qt 5.5.0+. Make include structure more consistent and update all apps for Qt 5.x. Start rework of ReportError, as used only for ReadDB and BuildDB. It's functionally complete for ReadDB, but not BuildDB, which still needs user input. Dropped BuildDB include from all apps not using it (i.e. all but KJVCanOpener).

programs/KJVCanOpener/Highlighter.cpp
@@ -30,7 +30,7 @@
 #include <QVariant>
 #include <QBrush>
 #include <QTextFormat>
-#if !defined(OSIS_PARSER_BUILD) && !defined(KJV_SEARCH_BUILD) && !defined(KJV_DIFF_BUILD)
+#ifndef IS_CONSOLE_APP
 #include <QPixmap>
 #include <QBitmap>
 #include <QPainter>
@@ -355,7 +355,7 @@ void CUserDefinedHighlighter::clearPhraseTags()
 // ============================================================================
 // ============================================================================
 
-#if !defined(OSIS_PARSER_BUILD) && !defined(KJV_SEARCH_BUILD) && !defined(KJV_DIFF_BUILD)
+#ifndef IS_CONSOLE_APP
 
 CHighlighterButtons::CHighlighterButtons(QObject *pParent)
 	:	QObject(pParent),

programs/KJVCanOpener/Highlighter.h
@@ -29,7 +29,7 @@
 #include <QObject>
 #include <QTextCharFormat>
 #include <QList>
-#if !defined(OSIS_PARSER_BUILD) && !defined(KJV_SEARCH_BUILD) && !defined(KJV_DIFF_BUILD)
+#ifndef IS_CONSOLE_APP
 #include <QKeySequence>
 #include <QToolBar>
 #include <QToolButton>
@@ -38,10 +38,10 @@
 #include <QWidgetAction>
 #include <QMouseEvent>
 #include <QShortcutEvent>
-#endif
 #include <QIcon>
 #include <QPointer>
 #include <QMenu>
+#endif
 
 // ============================================================================
 
@@ -261,7 +261,7 @@ private:
 // ============================================================================
 // ============================================================================
 
-#if !defined(OSIS_PARSER_BUILD) && !defined(KJV_SEARCH_BUILD) && !defined(KJV_DIFF_BUILD)
+#ifndef IS_CONSOLE_APP
 
 #define MAX_HIGHLIGHTER_NAME_SIZE 40				// Maximum number of characters in Highlighter Names
 

programs/KJVCanOpener/KJVSearchCriteria.cpp
@@ -27,7 +27,6 @@
 #include "ScriptureDocument.h"
 #include "Translator.h"
 #include "dbDescriptors.h"
-#include "VerseListModel.h"
 
 #include "BusyCursor.h"
 
@@ -600,7 +599,7 @@ QString CSearchWithinModel::toWebChannelHtml() const
 
 // ============================================================================
 
-#if !defined(OSIS_PARSER_BUILD) && !defined(KJV_SEARCH_BUILD) && !defined(KJV_DIFF_BUILD)
+#ifndef IS_CONSOLE_APP
 
 CKJVSearchCriteriaWidget::CKJVSearchCriteriaWidget(QWidget *parent) :
 	QWidget(parent),
@@ -700,7 +699,7 @@ void CKJVSearchCriteriaWidget::en_SearchWithinItemActivated(const QModelIndex &i
 		if ((ndxReference.isSet()) &&
 			(ndxReference != CSearchCriteria::SSI_COLOPHON) &&
 			(ndxReference != CSearchCriteria::SSI_SUPERSCRIPTION)) {
-			emit gotoIndex(CVerseListModel::navigationIndexFromLogicalIndex(ndxReference));
+			emit gotoIndex(CRelIndex::navigationIndexFromLogicalIndex(ndxReference));
 		}
 	}
 }

programs/KJVCanOpener/KJVSearchCriteria.h
@@ -29,12 +29,15 @@
 #include <QString>
 #include <QStringList>
 #include <QObject>
-#include <QWidget>
 #include <QAbstractItemModel>
 #include <QModelIndex>
 #include <QModelIndexList>
 #include <QList>
 #include <QMap>
+
+#ifndef IS_CONSOLE_APP
+#include <QWidget>
+#endif
 
 // ============================================================================
 
@@ -164,7 +167,7 @@ private:
 
 // ============================================================================
 
-#if !defined(OSIS_PARSER_BUILD) && !defined(KJV_SEARCH_BUILD) && !defined(KJV_DIFF_BUILD)
+#ifndef IS_CONSOLE_APP
 
 #include "ui_KJVSearchCriteria.h"
 

programs/KJVCanOpener/PersistentSettings.cpp
@@ -28,7 +28,9 @@
 #endif
 
 #include <QCoreApplication>
+#ifndef IS_CONSOLE_APP
 #include <QApplication>
+#endif
 
 #include <assert.h>
 

programs/KJVCanOpener/PhraseEdit.cpp
@@ -28,9 +28,10 @@
 #include "UserNotesDatabase.h"
 #include "ScriptureDocument.h"
 #include "PersistentSettings.h"
-#if !defined(OSIS_PARSER_BUILD) && !defined(KJV_SEARCH_BUILD) && !defined(KJV_DIFF_BUILD)
+#include "SearchCompleter.h"
+#ifndef IS_CONSOLE_APP
 #include "ToolTipEdit.h"
-#include "myApplication.h"
+#include <QToolTip>
 #endif
 #include "Translator.h"
 
@@ -38,7 +39,6 @@
 #include <QTextCharFormat>
 #include <QTextBlock>
 #include <QTextFragment>
-#include <QToolTip>
 #include <QPair>
 #include <QSet>
 
@@ -576,8 +576,10 @@ void CParsedPhrase::UpdateCompleter(const QTextCursor &curInsert, CSearchComplet
 {
 	ParsePhrase(curInsert);
 	FindWords();
+#ifndef IS_CONSOLE_APP
 	aCompleter.setFilterMatchString();
 	aCompleter.setWordsFromPhrase();
+#endif
 }
 
 QTextCursor CParsedPhrase::insertCompletion(const QTextCursor &curInsert, const QString& completion)
@@ -2921,6 +2923,8 @@ void CPhraseNavigator::removeAnchors()
 
 // ============================================================================
 
+#ifndef IS_CONSOLE_APP
+
 void CPhraseEditNavigator::selectWords(const TPhraseTag &tag)
 {
 	assert(!m_pBibleDatabase.isNull());
@@ -3151,6 +3155,8 @@ QString CPhraseNavigator::getToolTip(const CBibleDatabasePtr &pBibleDatabase, co
 
 	return strToolTip;
 }
+
+#endif
 
 // ============================================================================
 

programs/KJVCanOpener/PhraseEdit.h
@@ -30,7 +30,6 @@
 
 #include <QFlags>
 #include <QTextDocument>
-#include <QTextEdit>
 #include <QTextCursor>
 #include <QString>
 #include <QStringList>
@@ -38,6 +37,10 @@
 #include <QHelpEvent>
 #include <QList>
 #include <QSharedPointer>
+
+#ifndef IS_CONSOLE_APP
+#include <QTextEdit>
+#endif
 
 #include <assert.h>
 
@@ -641,6 +644,8 @@ Q_DECLARE_OPERATORS_FOR_FLAGS(CPhraseNavigator::TextRenderOptionFlags)
 
 // ============================================================================
 
+#ifndef IS_CONSOLE_APP
+
 class CPhraseEditNavigator : public CPhraseNavigator
 {
 	Q_OBJECT
@@ -669,6 +674,8 @@ private:
 	QTextEdit &m_TextEditor;
 	bool m_bUseToolTipEdit;			// True = Use CToolTipEdit instead of QToolTip
 };
+
+#endif
 
 // ============================================================================
 

programs/KJVCanOpener/SearchCompleter.cpp
@@ -29,8 +29,11 @@
 #include <QString>
 #include <QStringRef>
 #include <QRegExp>
-#include <QAbstractItemView>
 #include <QTimer>
+
+#ifndef IS_CONSOLE_APP
+#include <QAbstractItemView>
+#endif
 
 #include <assert.h>
 
@@ -328,6 +331,8 @@ void CSearchParsedPhraseListModel::setWordsFromPhrase(bool bForceUpdate)
 
 // ============================================================================
 
+#ifndef IS_CONSOLE_APP
+
 CSearchDictionaryListModel::CSearchDictionaryListModel(CDictionaryDatabasePtr pDictionary, const QTextEdit &editorWord, QObject *parent)
 	:	CSearchStringListModel(parent),
 		m_pDictionaryDatabase(pDictionary),
@@ -400,7 +405,11 @@ void CSearchDictionaryListModel::setWordsFromPhrase(bool bForceUpdate)
 //	emit modelChanged();
 }
 
+#endif
+
 // ============================================================================
+
+#ifndef IS_CONSOLE_APP
 
 CSearchCompleter::CSearchCompleter(const CParsedPhrase &parsedPhrase, QWidget *parentWidget)
 	:	QCompleter(parentWidget),
@@ -499,8 +508,8 @@ void CSearchCompleter::selectFirstMatchString()
 	QModelIndex indexFirstDecomposedWord = soundExFilterModel()->firstMatchStringIndex(false);
 
 	switch (completionFilterMode()) {
-		case CSearchCompleter::SCFME_NORMAL:
-		case CSearchCompleter::SCFME_SOUNDEX:
+		case SCFME_NORMAL:
+		case SCFME_SOUNDEX:
 			if (indexFirstComposedWord.isValid()) {
 				int nCompCount = completionModel()->rowCount();
 				for (int nComp = 0; nComp < nCompCount; ++nComp) {
@@ -516,7 +525,7 @@ void CSearchCompleter::selectFirstMatchString()
 				for (int nComp = 0; nComp < nCompCount; ++nComp) {
 					QModelIndex ndxComp = completionModel()->index(nComp, 0);
 					if (ndxComp.data(Qt::EditRole).toString().compare(indexFirstDecomposedWord.data(Qt::EditRole).toString()) == 0) {
-						if (completionFilterMode() == CSearchCompleter::SCFME_SOUNDEX) {
+						if (completionFilterMode() == SCFME_SOUNDEX) {
 							setCompletionPrefix(indexFirstDecomposedWord.data(Qt::DisplayRole).toString());		// Force assert a selection prefix or else it won't select it in this mode
 						}
 						popup()->setCurrentIndex(ndxComp);
@@ -526,7 +535,7 @@ void CSearchCompleter::selectFirstMatchString()
 				}
 			}
 			break;
-		case CSearchCompleter::SCFME_UNFILTERED:
+		case SCFME_UNFILTERED:
 			if (indexFirstComposedWord.isValid()) {
 				popup()->setCurrentIndex(indexFirstComposedWord);
 			} else if (indexFirstDecomposedWord.isValid()) {
@@ -550,6 +559,8 @@ void CSearchCompleter::setWordsFromPhrase(bool bForceUpdate)
 	assert(m_pSearchStringListModel != NULL);
 	m_pSearchStringListModel->setWordsFromPhrase(bForceUpdate);
 }
+
+#endif
 
 // ============================================================================
 

commit f8d3a1c1d984492267b209bf01cd841e9ee1078b

Fix webchannel build, by fixing include logic broken in 69852cc11df731d817e80b88944a0e04c3e1563f. Some of the things that were tagged for !IS_CONSOLE_APP should be QT_WIDGETS_LIB instead.

programs/KJVCanOpener/Highlighter.cpp
@@ -30,7 +30,7 @@
 #include <QVariant>
 #include <QBrush>
 #include <QTextFormat>
-#ifndef IS_CONSOLE_APP
+#ifdef QT_WIDGETS_LIB
 #include <QPixmap>
 #include <QBitmap>
 #include <QPainter>
@@ -355,7 +355,7 @@ void CUserDefinedHighlighter::clearPhraseTags()
 // ============================================================================
 // ============================================================================
 
-#ifndef IS_CONSOLE_APP
+#ifdef QT_WIDGETS_LIB
 
 CHighlighterButtons::CHighlighterButtons(QObject *pParent)
 	:	QObject(pParent),

programs/KJVCanOpener/Highlighter.h
@@ -29,7 +29,7 @@
 #include <QObject>
 #include <QTextCharFormat>
 #include <QList>
-#ifndef IS_CONSOLE_APP
+#ifdef QT_WIDGETS_LIB
 #include <QKeySequence>
 #include <QToolBar>
 #include <QToolButton>
@@ -261,7 +261,7 @@ private:
 // ============================================================================
 // ============================================================================
 
-#ifndef IS_CONSOLE_APP
+#ifdef QT_WIDGETS_LIB
 
 #define MAX_HIGHLIGHTER_NAME_SIZE 40				// Maximum number of characters in Highlighter Names
 

programs/KJVCanOpener/KJVCanOpener.pro
@@ -61,7 +61,7 @@ ios {
 
 exceptions_off:DEFINES += NOT_USING_EXCEPTIONS
 
-!android:!ios:!emscripten:!vnc {
+!console:!android:!ios:!emscripten:!vnc {
 	CONFIG += buildKJVDatabase
 	DEFINES += BUILD_KJV_DATABASE
 }

programs/KJVCanOpener/KJVSearchCriteria.cpp
@@ -599,7 +599,7 @@ QString CSearchWithinModel::toWebChannelHtml() const
 
 // ============================================================================
 
-#ifndef IS_CONSOLE_APP
+#ifdef QT_WIDGETS_LIB
 
 CKJVSearchCriteriaWidget::CKJVSearchCriteriaWidget(QWidget *parent) :
 	QWidget(parent),

programs/KJVCanOpener/KJVSearchCriteria.h
@@ -35,7 +35,7 @@
 #include <QList>
 #include <QMap>
 
-#ifndef IS_CONSOLE_APP
+#ifdef QT_WIDGETS_LIB
 #include <QWidget>
 #endif
 
@@ -167,7 +167,7 @@ private:
 
 // ============================================================================
 
-#ifndef IS_CONSOLE_APP
+#ifdef QT_WIDGETS_LIB
 
 #include "ui_KJVSearchCriteria.h"
 

programs/KJVCanOpener/PersistentSettings.cpp
@@ -28,7 +28,7 @@
 #endif
 
 #include <QCoreApplication>
-#ifndef IS_CONSOLE_APP
+#ifdef QT_WIDGETS_LIB
 #include <QApplication>
 #endif
 

programs/KJVCanOpener/PhraseEdit.cpp
@@ -29,7 +29,7 @@
 #include "ScriptureDocument.h"
 #include "PersistentSettings.h"
 #include "SearchCompleter.h"
-#ifndef IS_CONSOLE_APP
+#ifdef QT_WIDGETS_LIB
 #include "ToolTipEdit.h"
 #include <QToolTip>
 #endif
@@ -576,7 +576,7 @@ void CParsedPhrase::UpdateCompleter(const QTextCursor &curInsert, CSearchComplet
 {
 	ParsePhrase(curInsert);
 	FindWords();
-#ifndef IS_CONSOLE_APP
+#ifdef QT_WIDGETS_LIB
 	aCompleter.setFilterMatchString();
 	aCompleter.setWordsFromPhrase();
 #endif
@@ -2923,7 +2923,7 @@ void CPhraseNavigator::removeAnchors()
 
 // ============================================================================
 
-#ifndef IS_CONSOLE_APP
+#ifdef QT_WIDGETS_LIB
 
 void CPhraseEditNavigator::selectWords(const TPhraseTag &tag)
 {

programs/KJVCanOpener/PhraseEdit.h
@@ -38,7 +38,7 @@
 #include <QList>
 #include <QSharedPointer>
 
-#ifndef IS_CONSOLE_APP
+#ifdef QT_WIDGETS_LIB
 #include <QTextEdit>
 #endif
 
@@ -644,7 +644,7 @@ Q_DECLARE_OPERATORS_FOR_FLAGS(CPhraseNavigator::TextRenderOptionFlags)
 
 // ============================================================================
 
-#ifndef IS_CONSOLE_APP
+#ifdef QT_WIDGETS_LIB
 
 class CPhraseEditNavigator : public CPhraseNavigator
 {

programs/KJVCanOpener/SearchCompleter.cpp
@@ -31,7 +31,7 @@
 #include <QRegExp>
 #include <QTimer>
 
-#ifndef IS_CONSOLE_APP
+#ifdef QT_WIDGETS_LIB
 #include <QAbstractItemView>
 #endif
 
@@ -331,7 +331,7 @@ void CSearchParsedPhraseListModel::setWordsFromPhrase(bool bForceUpdate)
 
 // ============================================================================
 
-#ifndef IS_CONSOLE_APP
+#ifdef QT_WIDGETS_LIB
 
 CSearchDictionaryListModel::CSearchDictionaryListModel(CDictionaryDatabasePtr pDictionary, const QTextEdit &editorWord, QObject *parent)
 	:	CSearchStringListModel(parent),
@@ -409,7 +409,7 @@ void CSearchDictionaryListModel::setWordsFromPhrase(bool bForceUpdate)
 
 // ============================================================================
 
-#ifndef IS_CONSOLE_APP
+#ifdef QT_WIDGETS_LIB
 
 CSearchCompleter::CSearchCompleter(const CParsedPhrase &parsedPhrase, QWidget *parentWidget)
 	:	QCompleter(parentWidget),

programs/KJVCanOpener/SearchCompleter.h
@@ -34,7 +34,7 @@
 #include <QMap>
 #include <QStringList>
 
-#ifndef IS_CONSOLE_APP
+#ifdef QT_WIDGETS_LIB
 #include <QWidget>
 #include <QCompleter>
 #include <QTextEdit>
@@ -127,7 +127,7 @@ private:
 
 // ============================================================================
 
-#ifndef IS_CONSOLE_APP
+#ifdef QT_WIDGETS_LIB
 
 class CSearchDictionaryListModel : public CSearchStringListModel
 {
@@ -231,7 +231,7 @@ private:
 // ============================================================================
 
 
-#ifdef IS_CONSOLE_APP
+#ifndef QT_WIDGETS_LIB
 
 class CSearchCompleter
 {