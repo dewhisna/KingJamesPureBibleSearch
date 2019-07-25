4032a6d0990137b521ad4969f20371e30da637a1
Donna Whisnant <dewhisna@users.noreply.github.com>
2019-07-24 22:27:25 -0500

    Update KJPBS Docker Provisioning. Upgrade from Ubuntu 16.04 Xenial to Ubuntu 18.04 Bionic. Use precompiled Qt instead of building it from source. Build all KJPBS tools. Extend the container for GUI support with XRDP and Xfce desktop. Add multi-user systemd init support with https://github.com/gdraheim/docker-systemctl-replacement.git Update instructions for all new features and a persistent container.

--- docker/README.txt
+++ docker/README.txt
@@ -1,5 +1,5 @@
-How to compile KingJamesPureBibleSearch Command-Line Utilities Using a
-Docker Container provisioned by Packer:
+How to compile KingJamesPureBibleSearch GUI, WebChannel, and Command-Line Utilities
+Using a Docker Container Image provisioned by Packer:
 
 1. Install Docker
 2. Install Packer
@@ -7,9 +7,13 @@ Docker Container provisioned by Packer:
 4. Run KJPBS Tools
 
 KJPBS Tools Built:
+- KingJamesPureBibleSearch
+- KJVDataParse
+- KJVDictWord
+- KJVDiff
 - KJVLookup
-- KJVSearch
 - KJVPhraseSearch
+- KJVSearch
 - webchannel
 
 
@@ -51,7 +55,7 @@ $ sudo apt-get update
 $ sudo apt-get install docker-ce
 
 Add yourself to the Docker Group:
-$ sudo usermod -aG docker your-user
+$ sudo usermod -aG docker your-user-id
 (Logout and back in to enable group)
 
 Verify Installation:
@@ -67,7 +71,7 @@ Install Packer
 Download latest Packer zip file from:
 https://www.packer.io/downloads.html
 
-Unzip and:
+Unzip it and copy it to a folder on your system search path:
 $ sudo cp packer /usr/local/bin
 
 ================================================================================
@@ -77,9 +81,15 @@ $ sudo cp packer /usr/local/bin
 Build KJPBS Docker Image
 ================================================================================
 
+You can either do a 'git clone' of the entire KJPBS Repository, or you can
+just download the contents of the 'docker' subfolder.  If you do the latter,
+be sure to navigate to the 'raw' file content and don't accidentally save
+the GitHub generated webpage rendered view of the files or it won't work.
+
 $ cd KingJamesPureBibleSearch/docker
 (from Git clone location)
 
+Run Packer to generate the Docker Image:
 $ packer build packer_docker_KJPBS.json
 (and take a coffee break... this step takes a while)
 
@@ -90,21 +100,256 @@ $ packer build packer_docker_KJPBS.json
 Run KJPBS Tools
 ================================================================================
 
-Start the Docker container:
-$ docker run --rm -it localhost:5000/dewtronics/kjpbs-cmd:latest
+Once you have created the image, start a new Docker container from this image
+using:
+$ docker run -d --restart=always --name kjpbs localhost:5000/dewtronics/kjpbs:latest
 
-Now, run the tools.  For example:
-# KJVLookup 1 "John 3:16"
+This will start a new container named 'kjpbs' running in the background.
 
-# KJVSearch -h -t 1 "Word of God"
+To login to the container, use this command:
+$ docker exec -it kjpbs startup
 
-# KJVPhraseSearch -sc 1 13
+This will execute the special startup script inside the container named 'kjpbs' in
+an interactive terminal.  When starting the container in this fashion it should
+not prompt for a password and most operations you do with the KJPBS tools will
+not require any password.
 
-Run them without any arguments for usage information.
+However, the initial password for the 'kjpbs' user in the Docker container is
+an empty string.  So, if you want to experiment with other container features,
+such as log into the container with RDP (Remote Desktop Protocol) to run the
+GUI version of KJPBS inside the container, or if you need to use any of the
+'sudo' commands, just press enter if prompted for a password to enter an empty
+string.
 
-Enter 'exit' when finished to shutdown the container:
-# exit
+If you wish to actually set a non-empty password for some reason, use the
+'passwd' command within the container.  But note that it will remain set only as
+long as the container exists.  If you remove the container, any settings inside
+the container are lost.
+
+Now that you've logged into the container, you can run the tools.  For example:
+$ KJVLookup 1 "John 3:16"
+
+$ KJVSearch -h -t 1 "Word of God"
+
+$ KJVPhraseSearch -sc 1 13
+
+Run these commands without any arguments for usage information and help
+instructions.
+
+Enter 'exit' when finished to exit your login shell to the container:
+$ exit
+
+
+This will only exit your login, but the Docker Container will remain running
+in the background.  You can list the containers and check their status using:
+$ docker container ls --all
+
+
+And you can log back into this container anytime with the 'exec' command:
+$ docker exec -it kjpbs startup 
+
+
+You can stop the KJPBS Docker Container with:
+$ docker stop kjpbs
+
+
+And you can remove the KJPBS Docker Container (after stopping it) with:
+$ docker rm kjpbs
+
+
+Note that once you've created the KJPBS Docker Image, as long as you don't
+remove it (even if you stop and remove all containers that were running the
+image), there is no need to regenerate the entire image each time you want
+to use KJPBS.  You can just start with this step of "Running KJPBS Tools"
+with a Docker Container.
+
+You can list the installed Docker Images with:
+$ docker images
+
+
+Later on, if you decide to upgrade your KJPBS Docker Image to get a new
+version of KJPBS and its tools, don't forget to remove any existing KJPBS
+containers and images or you will end up wasting space on your drive with
+old images that you no longer use.
+
+Stop and remove just the kjpbs container with:
+$ docker stop kjpbs
+$ docker rm kjpbs
+
+And remove the old image with:
+$ docker image rm localhost:5000/dewtronics/kjpbs:latest
+
+
+If you are only using Docker for running KJPBS, you can remove all old
+containers using:
+$ docker container prune
+
+And then remove all old images with:
+$ docker image prune
+
+Note that until you remove the images, you can still use them to launch
+new containers.  So make sure you are done with an image before deleting it.
 
 ================================================================================
 
+
+================================================================================
+Run KJPBS WebChannel and the full KJPBS GUI inside the container
+================================================================================
+
+Launching KJPBS Web Channel:
+----------------------------
+After you've created your KJPBS Docker Image, if you want to run the KJPBS
+WebChannel locally on your PC from within the container, you will need to first
+stop and remove any containers you have launched with the instructions above with:
+$ docker stop kjpbs
+$ docker rm kjpbs
+
+The reason, to stop the container is to launch it with the web ports mapped to
+your host PC's localhost to easily access it from your browser.  So start the
+container with both ports 80 and 9340 mapped to your host computer:
+$ docker run -p 80:80 -p 9340:9340 -d --restart=always --name kjpbs localhost:5000/dewtronics/kjpbs:latest
+
+This assumes you aren't currently using port 80 on your host PC.  If you are,
+replace the first '80' with something else like '8080':
+$ docker run -p 8080:80 -p 9340:9340 -d --restart=always --name kjpbs localhost:5000/dewtronics/kjpbs:latest
+
+(Note port 9340 could also be remapped to something different, but then you'd
+have to pass that to the WebChannel web page when launching it.  Since it's not
+likely port 9340 would already be in use, those instructions aren't included here)
+
+
+Then login to the container:
+$ docker exec -it kjpbs startup
+
+And start the KJPBS WebChannel server:
+$ webchannel
+
+Bring up your favorite Web Browser on your PC and navigate to:
+http://localhost/
+
+Or:
+http://localhost:8080/
+If you remapped to port 8080 above (or whatever number you used instead).
+
+And you should now be running the full WebChannel application through your browser.
+
+To stop the webchannel, you can press ctrl-C in the terminal where you ran
+'webchannel' from.  Or, you could use the "more correct" method by sending a
+SIGHUP (hang-up signal) to the webchannel process either from another login
+session with the container.  Or by an 'exec' command:
+$ docker exec kjpbs kill -SIGHUP `docker exec kjpbs pidof KingJamesPureBibleSearch_webchannel`
+
+If for some reason the webchannel page doesn't load in your browser, you can try
+to restart the 'nginx' webserver in the container by logging into the container
+in another terminal window and using:
+$ sudo nginx -s stop
+$ sudo nginx
+
+And then trying again to access the page.  You should not have to restart the
+KJPBS WebChannel app with nginx, as it runs as an independent server and was
+started by you manually with the 'webchannel' command, whereas the 'nginx' server
+is started automatically as a SystemD daemon (see notes below for KJPBS GUI).
+
+It probably won't even be necessary to restart 'nginx' because, for some reason,
+it seems to be a well behaved daemon with respect to Docker Containers and the
+docker-systemctl-replacement script I'm using to autolaunch multi-user system
+daemons.
+
+
+Running KJPBS GUI:
+------------------
+First install a suitable RDP client on your host PC.  For Linux, KRDC is a decent
+client.  Windows comes with a 'Remote Desktop Connection' application.  And Mac
+has a number of freely available clients as well.
+
+After you've created your KJPBS Docker Image, if you want to run the KJPBS GUI
+locally on your PC from within the Container using RDP, you will need to first
+stop and remove any containers you have launched with the instructions above with:
+$ docker stop kjpbs
+$ docker rm kjpbs
+
+The reason, to stop the container is to launch it with the 3389 RDP port mapped to
+your host PC's localhost to easily access it from your host PC.  So start the container with:
+$ docker run -p 3389:3389 -d --restart=always --name kjpbs localhost:5000/dewtronics/kjpbs:latest
+
+This assumes that you aren't currently using port 3389 on your host PC.  If you are,
+replace the first 3389 with a different port, and specify that port to your RDP
+Client.  For example:
+$ docker run -p 8000:3389 -d --restart=always --name kjpbs localhost:5000/dewtronics/kjpbs:latest
+
+Then login to the container:
+$ docker exec -it kjpbs startup
+
+The next step is a little tricky and is only because it's difficult to handle the
+startup/shutdown logic for running multi-user SystemD daemons inside a Docker
+Container.
+
+Check the system processes:
+$ ps auxww
+
+And make sure you see both '/usr/sbin/xrdp' and '/usr/sbin/xrdp-sesman' running.
+If only one of the two is running or neither is running, first stop xrdp with:
+$ sudo systemctl stop xrdp
+$ sudo systemctl stop xrdp-sesman
+
+Then start the xrdp server with:
+$ sudo /etc/init.d/xrdp
+
+Now when you check the system processes, you should see both listed.
+
+Then, bring up your RDP Client on your host PC and connect to:
+kjpbs@localhost:3389
+Using the RDP protocol (replace the 3389 with whatever number you binded above
+when running the container, if you used a number other than 3389).
+
+Depending on your RDP client, the above may be entered in a single connection
+string, or you may have to enter them as separate fields.  The username is 'kjpbs'.
+The URL is 'localhost'.  And the port is '3389'.  And if prompted for a password,
+the initial password for 'kjpbs' in the container is an empty string, so leave
+it blank (unless you manually changed it with the 'passwd' command in the container).
+
+It's recommended that you use at least a 1280x1024 display size setting on your
+RDP client (or larger).  Once logged in, select to create the 'default' taskbar
+and you'll be looking at the Xubuntu Xfce desktop.  Most of the applications listed
+in the menu will NOT work.  So don't be surprised if you get an error when you
+select them.  This Docker Container Image is quite sparse and only contains the
+essential pieces for KJPBS.  If you want the other apps to be available, you'll
+need to manually use 'sudo apt-get install' from the container's command-line
+to install them.
+
+To Launch KJPBS, select "Run Program..." from the Appplications Menu drop list
+in the upper-left-hand corner, and enter: "KingJamesPureBibleSearch".
+This will launch the complete KJPBS GUI for Linux as if you had installed it
+directly on your own PC, only it's running completely inside the Docker
+Container, much like a Virtual Machine.
+
+You have to manually enter 'KingJamesPureBibleSearch' in the 'Run Program...'
+window because there is currently no icon installed in the menus for KJPBS,
+since this Docker image was a raw source build of KJPBS and not from the
+installer.
+
+When done, either 'logout' from the Xfce desktop or just drop the RDP
+connection.  Note that if you just drop the RDP connection, but stay logged
+in with KJPBS and such running, it will still be running right where you left
+off when you reconnect -- unless you shutdown or otherwise disturb the Docker
+Container.
+
+================================================================================
+
+
+================================================================================
+Run Everything
+================================================================================
+
+If you want to be able to run all of the tools at the same time, note that you
+can combine the instructions from above and start your container with all
+needed ports mapped to your localhost at the same time, by using:
+
+$ docker run -p 80:80 -p 9340:9340 -p 3389:3389 -d --restart=always --name kjpbs localhost:5000/dewtronics/kjpbs:latest
+
+That way there will be no needed to stop and remove the existing container and
+map different ports.  You'll have one container that can run everything.
+
+================================================================================
 
--- docker/docker_build_KJPBS.sh
+++ docker/docker_build_KJPBS.sh
@@ -4,16 +4,24 @@ set -ex
 
 export DEBIAN_FRONTEND=noninteractive
 
-export BUILD_HOME=/root
+export USER=kjpbs
+
+export BUILD_HOME=/home/$USER
 export INSTALL_DIR=/usr/local/bin/
 
 export QT=$BUILD_HOME/Qt
-export QT_SRC=$QT/5.6.3-src
-export QT_BUILD=$QT/5.6.3-build
-export QT_DIR=$QT/5.6.3
-export KJPBS_BUILD=KJVPhraseSearch
+export QT_DIR=$QT/5.13.0/5.13.0/gcc_64
 
 export PROJECT_DIR=$BUILD_HOME/Documents/programs/Bible
+export KJPBS_BUILD=gcc_64
+
+
+# ------------------
+# Create KJPBS User and VNC startup users:
+# ------------------
+useradd -m -U -G adm,dialout,cdrom,sudo,dip,plugdev -s /bin/bash kjpbs
+echo kjpbs:U6aMy0wojraho | chpasswd -e
+#passwd --expire kjpbs
 
 
 # ---------------------
@@ -21,28 +29,53 @@ export PROJECT_DIR=$BUILD_HOME/Documents/programs/Bible
 # ---------------------
 apt-get update
 apt-get install -y software-properties-common
-sed -i '/^#\s*deb-src /s/^#\s*//' "/etc/apt/sources.list"
+#sed -i '/^#\s*deb-src /s/^#\s*//' "/etc/apt/sources.list"
 add-apt-repository -y ppa:maxmind/ppa
 apt-get update
 apt-get install -y build-essential
-apt-get install -y libmaxminddb0 libmaxminddb-dev mmdb-bin geoipupdate
-# Downgrade Wayland support so libqt5* build-deps will resolve (since stock 14.04 LTS and 16.04 LTS isn't Wayland):
-#apt-get install -y libopenvg1-mesa    # 14.04
-apt-get install -y libopenvg1-mesa-lts-utopic    # 16.04
-# Easiest to slurp in Qt5 build dependencies, since we are building Qt5 as a pre-requisite:
-apt-get build-dep -y libqt5core5a libqt5network5 libqt5widgets5
+apt-get install -y pkg-config
+apt-get install -y cmake
 apt-get install -y gperf bison
-# The following should be redundant, as they should be part of the build-dep above, but are essential:
-apt-get install -y libxcb1 libxcb1-dev libx11-xcb1 libx11-xcb-dev libxcb-keysyms1 libxcb-keysyms1-dev libxcb-image0 libxcb-image0-dev libxcb-shm0 libxcb-shm0-dev libxcb-icccm4 libxcb-icccm4-dev libxcb-sync-dev libxcb-xfixes0-dev libxrender-dev libxcb-shape0-dev
-apt-get install -y libfreetype6-dev fontconfig
-apt-get install -y libjpeg-dev
-apt-get install -y zlib1g zlib1g-dev
+apt-get install -y libmaxminddb0 libmaxminddb-dev mmdb-bin geoipupdate
+apt-get install -y libgl1-mesa-dev
+# Easiest to slurp in Qt5 build dependencies, since we are building Qt5 as a pre-requisite:
+# apt-get build-dep -qq --yes libqt5core5a libqt5gui5 libqt5widgets5 libqt5network5 libqt5webchannel5 libqt5sql5
+apt-get install -y libqt5core5a libqt5gui5 libqt5widgets5 libqt5network5 libqt5webchannel5 libqt5sql5
 # A few helpers:
 apt-get install -y perl python
 apt-get install -y binfmt-support
 apt-get install -y wget
 apt-get install -y curl git xz-utils
 apt-get install -y nginx
+apt-get install -y joe
+apt-get install -y sudo
+
+# This is for remote desktop to run KJPBS
+apt-get install -y xrdp xfce4
+echo xfce4-session >$BUILD_HOME/.xsession
+sed -i 's/^\(LogLevel\s*=\s*\).*$/\1WARNING/' /etc/xrdp/xrdp.ini
+sed -i 's/^\(SyslogLevel\s*=\s*\).*$/\1WARNING/' /etc/xrdp/xrdp.ini
+sed -i 's/^\(EnableSyslog\s*=\s*\).*$/\1false/' /etc/xrdp/xrdp.ini
+
+# Setup Container Startup
+(
+cat <<'EOF'
+#!/bin/bash
+
+su -l kjpbs
+EOF
+) > /bin/startup
+chmod 755 /bin/startup
+
+cd $BUILD_HOME
+git clone https://github.com/gdraheim/docker-systemctl-replacement.git
+cd docker-systemctl-replacement
+git checkout 73b5aff2ba6abfd254d236f1df22ff4971d44660    # v1.4.3245
+mv /bin/systemctl /bin/systemctl.orig
+cp files/docker/systemctl.py /bin/systemctl
+chmod 755 /bin/systemctl
+cd $BUILD_HOME
+rm -rf docker-systemctl-replacement
 
 
 # ---------------------
@@ -51,53 +84,42 @@ apt-get install -y nginx
 cd $BUILD_HOME
 mkdir -p $PROJECT_DIR
 cd $PROJECT_DIR
-git clone https://github.com/dewhisna/KingJamesPureBibleSearch.git
+git clone --depth 50 https://github.com/dewhisna/KingJamesPureBibleSearch.git
 cd KingJamesPureBibleSearch
 sed -i 's/git@github.com:/https:\/\/github.com\//' .gitmodules
 git submodule update --init --recursive
 
 
 # ---------------------
-# Get Qt:
+# Get prebuilt Qt:
 # ---------------------
-mkdir -p $QT
-cd $QT
-wget --quiet https://download.qt.io/official_releases/qt/5.6/5.6.3/single/qt-everywhere-opensource-src-5.6.3.tar.xz
-tar -xf qt-everywhere-opensource-src-5.6.3.tar.xz
-mv qt-everywhere-opensource-src-5.6.3 $QT_SRC
-
-
-# ---------------------
-# Build Qt for building KJPBS console mode:
-# ---------------------
-mkdir -p $QT_BUILD
-cd $QT_BUILD
-# Note: Qt 5.5.1 (unlike 5.5.0 and before) is setup for Wayland and so we can't use -libinput
-# $QT_SRC/configure -v -release -qt-zlib -qt-libpng -qt-libjpeg -qt-sql-sqlite -qt-freetype -qt-pcre -qt-xcb -qt-xkbcommon-x11 -libinput -fontconfig -directfb -linuxfb -nomake examples -nomake tests -opensource -confirm-license -prefix "$QT_DIR"
-$QT_SRC/configure -v -release -qt-zlib -qt-libpng -qt-libjpeg -qt-sql-sqlite -qt-freetype -qt-pcre -qt-xcb -qt-xkbcommon-x11 -fontconfig -directfb -linuxfb -nomake examples -nomake tests -opensource -confirm-license -prefix "$QT_DIR"
-make -j 4
-make install
-
-
+# From: http://download.qt-project.org/official_releases/qt/5.13/5.13.0/
+#   Or: https://download.qt.io/official_releases/qt/5.13/5.13.0/
+cd $BUILD_HOME
+wget --no-verbose -nc http://vnc.purebiblesearch.com/c5ee798b-c224-4d6e-b457-1c648a99a3a8/Qt_5.13.0_gcc_64.tar.xz
+echo '155e0b96430e41f24b7b70d7d55e58a850f0c6b0  Qt_5.13.0_gcc_64.tar.xz' | sha1sum -c
+tar -xf Qt_5.13.0_gcc_64.tar.xz
+chown -R $USER:$USER Qt/
 # ---------------------
 # Link Qt5 Source -> Qt4 Paths used in KJPBS:
+#  This has already been applied to the prebuilt binary
+#  above.  If building a custom Qt, apply the follow to
+#  copy the 3rdparty/zlib* source folders (from the Qt
+#  source code) to the Qt binary folder.
 # ---------------------
-cd $QT_DIR
-mkdir -p src/3rdparty
-cd src/3rdparty/
-cp -r $QT_SRC/qtbase/src/3rdparty/zlib* .
-cd $QT_DIR
-mkdir -p src/gui/mac/qt_menu.nib
-cd src/gui/mac/qt_menu.nib
-cp -r $QT_SRC/qtbase/src/plugins/platforms/cocoa/qt_menu.nib/* .
+# cd $$[QT_INSTALL_PREFIX]
+# mkdir -p src/3rdparty
+# cd src/3rdparty/
+# cp -r $QT_SRC/qtbase/src/3rdparty/zlib* .
+# ---------------------
 
 
 # ---------------------
 # Build QtStylePlugins:
 # ---------------------
 cd $PROJECT_DIR/KingJamesPureBibleSearch/programs
-mkdir build-qtstyleplugins_gcc_64
-cd build-qtstyleplugins_gcc_64
+mkdir -p build-qtstyleplugins_$KJPBS_BUILD
+cd build-qtstyleplugins_$KJPBS_BUILD
 $QT_DIR/bin/qmake CONFIG+=static CONFIG+=release CONFIG+=force-debug-info ../qtstyleplugins/qtstyleplugins.pro
 make -j 4
 make install
@@ -106,9 +128,9 @@ make install
 # ---------------------
 # Build wwWidgets:
 # ---------------------
-cd $QT_DIR/..
-mkdir -p build-wwwidgets4_gcc_64/Release
-cd build-wwwidgets4_gcc_64/Release
+cd $QT
+mkdir -p build-wwwidgets4_$KJPBS_BUILD/Release
+cd build-wwwidgets4_$KJPBS_BUILD/Release
 $QT_DIR/bin/qmake CONFIG+=release $PROJECT_DIR/KingJamesPureBibleSearch/support/wwWidgets/wwwidgets4.pro
 make -j 4
 make install
@@ -116,39 +138,94 @@ cp $PROJECT_DIR/KingJamesPureBibleSearch/support/wwWidgets/translations/wwwidget
 
 
 # ---------------------
-# Create target hierarchy folder:
+# Create target hierarchy folders:
 # ---------------------
 mkdir -p $BUILD_HOME/KJVCanOpener/app
+mkdir -p $BUILD_HOME/KJVDataParse
 
 
 # ---------------------
-# Configure and Build KJPBS webchannel:
+# Configure and Build KJPBS GUI:
 # ---------------------
-cd $PROJECT_DIR/KingJamesPureBibleSearch/programs/
-mkdir -p build-KJVCanOpener_gcc_64_webchannel/Release
-cd build-KJVCanOpener_gcc_64_webchannel/Release
-$QT_DIR/bin/qmake CONFIG+=release CONFIG+=console CONFIG+=nospeech CONFIG+=webchannel ../../KJVCanOpener/KJVCanOpener.pro
+mkdir -p $PROJECT_DIR/KingJamesPureBibleSearch/programs/build-KJVCanOpener_$KJPBS_BUILD/Release
+cd $PROJECT_DIR/KingJamesPureBibleSearch/programs/build-KJVCanOpener_$KJPBS_BUILD/Release
+$QT_DIR/bin/qmake CONFIG+=release CONFIG+=nospeech ../../KJVCanOpener/KJVCanOpener.pro
 make -j 4
 cp KingJamesPureBibleSearch $BUILD_HOME/KJVCanOpener/app/
 ln -s $BUILD_HOME/KJVCanOpener/app/KingJamesPureBibleSearch $INSTALL_DIR/KingJamesPureBibleSearch
+
+
+# ---------------------
+# Configure and Build KJPBS WebChannel:
+# ---------------------
+mkdir -p $PROJECT_DIR/KingJamesPureBibleSearch/programs/build-KJVCanOpener_webchannel_$KJPBS_BUILD/Release
+cd $PROJECT_DIR/KingJamesPureBibleSearch/programs/build-KJVCanOpener_webchannel_$KJPBS_BUILD/Release
+$QT_DIR/bin/qmake CONFIG+=release CONFIG+=console CONFIG+=nospeech CONFIG+=webchannel ../../KJVCanOpener/KJVCanOpener.pro
+make -j 4
+cp KingJamesPureBibleSearch $BUILD_HOME/KJVCanOpener/app/KingJamesPureBibleSearch_webchannel
+ln -s $BUILD_HOME/KJVCanOpener/app/KingJamesPureBibleSearch_webchannel $INSTALL_DIR/KingJamesPureBibleSearch_webchannel
 
 (
 cat <<'EOF'
 #!/bin/bash
 
-KingJamesPureBibleSearch -webchannel 9340,0.0.0.0
+KingJamesPureBibleSearch_webchannel -webchannel 9340,0.0.0.0
 EOF
 ) > $INSTALL_DIR/webchannel
 chmod 755 $INSTALL_DIR/webchannel
 
-cp -r $PROJECT_DIR/KingJamesPureBibleSearch/programs/build-KJVCanOpener_gcc_64_webchannel/Release/html/* /var/www/html/
+cp -r $PROJECT_DIR/KingJamesPureBibleSearch/programs/build-KJVCanOpener_webchannel_$KJPBS_BUILD/Release/html/* /var/www/html/
+
+
+# ---------------------
+# Configure and Build KJVDataParse:
+# ---------------------
+mkdir -p $PROJECT_DIR/KingJamesPureBibleSearch/programs/build-KJVDataParse_$KJPBS_BUILD/Release
+cd $PROJECT_DIR/KingJamesPureBibleSearch/programs/build-KJVDataParse_$KJPBS_BUILD/Release
+$QT_DIR/bin/qmake CONFIG+=release ../../KJVDataParse/KJVDataParse.pro
+make -j 4
+cp KJVDataParse $BUILD_HOME/KJVCanOpener/app/
+ln -s $BUILD_HOME/KJVCanOpener/app/KJVDataParse $INSTALL_DIR/KJVDataParse
+
+
+# ---------------------
+# Configure and Build KJVDictWord:
+# ---------------------
+mkdir -p $PROJECT_DIR/KingJamesPureBibleSearch/programs/build-KJVDictWord_$KJPBS_BUILD/Release
+cd $PROJECT_DIR/KingJamesPureBibleSearch/programs/build-KJVDictWord_$KJPBS_BUILD/Release
+$QT_DIR/bin/qmake CONFIG+=release ../../KJVDictWord/KJVDictWord.pro
+make -j 4
+cp KJVDictWord $BUILD_HOME/KJVCanOpener/app/
+ln -s $BUILD_HOME/KJVCanOpener/app/KJVDictWord $INSTALL_DIR/KJVDictWord
+
+
+# ---------------------
+# Configure and Build KJVDiff:
+# ---------------------
+mkdir -p $PROJECT_DIR/KingJamesPureBibleSearch/programs/build-KJVDiff_$KJPBS_BUILD/Release
+cd $PROJECT_DIR/KingJamesPureBibleSearch/programs/build-KJVDiff_$KJPBS_BUILD/Release
+$QT_DIR/bin/qmake CONFIG+=release ../../KJVDiff/KJVDiff.pro
+make -j 4
+cp KJVDiff $BUILD_HOME/KJVCanOpener/app/
+ln -s $BUILD_HOME/KJVCanOpener/app/KJVDiff $INSTALL_DIR/KJVDiff
+
+
+# ---------------------
+# Configure and Build KJVLookup:
+# ---------------------
+mkdir -p $PROJECT_DIR/KingJamesPureBibleSearch/programs/build-KJVLookup_$KJPBS_BUILD/Release
+cd $PROJECT_DIR/KingJamesPureBibleSearch/programs/build-KJVLookup_$KJPBS_BUILD/Release
+$QT_DIR/bin/qmake CONFIG+=release ../../KJVLookup/KJVLookup.pro
+make -j 4
+cp KJVLookup $BUILD_HOME/KJVCanOpener/app/
+ln -s $BUILD_HOME/KJVCanOpener/app/KJVLookup $INSTALL_DIR/KJVLookup
+
 
 # ---------------------
 # Configure and Build KJVPhraseSearch:
 # ---------------------
-cd $PROJECT_DIR/KingJamesPureBibleSearch/programs/
-mkdir -p build-KJVPhraseSearch/Release
-cd build-KJVPhraseSearch/Release
+mkdir -p $PROJECT_DIR/KingJamesPureBibleSearch/programs/build-KJVPhraseSearch_$KJPBS_BUILD/Release
+cd $PROJECT_DIR/KingJamesPureBibleSearch/programs/build-KJVPhraseSearch_$KJPBS_BUILD/Release
 $QT_DIR/bin/qmake CONFIG+=release ../../KJVPhraseSearch/KJVPhraseSearch.pro
 make -j 4
 cp KJVPhraseSearch $BUILD_HOME/KJVCanOpener/app/
@@ -158,9 +235,8 @@ ln -s $BUILD_HOME/KJVCanOpener/app/KJVPhraseSearch $INSTALL_DIR/KJVPhraseSearch
 # ---------------------
 # Configure and Build KJVSearch:
 # ---------------------
-cd $PROJECT_DIR/KingJamesPureBibleSearch/programs/
-mkdir -p build-KJVSearch/Release
-cd build-KJVSearch/Release
+mkdir -p $PROJECT_DIR/KingJamesPureBibleSearch/programs/build-KJVSearch_$KJPBS_BUILD/Release
+cd $PROJECT_DIR/KingJamesPureBibleSearch/programs/build-KJVSearch_$KJPBS_BUILD/Release
 $QT_DIR/bin/qmake CONFIG+=release ../../KJVSearch/KJVSearch.pro
 make -j 4
 cp KJVSearch $BUILD_HOME/KJVCanOpener/app/
@@ -168,19 +244,7 @@ ln -s $BUILD_HOME/KJVCanOpener/app/KJVSearch $INSTALL_DIR/KJVSearch
 
 
 # ---------------------
-# Configure and Build KJVLookup:
-# ---------------------
-cd $PROJECT_DIR/KingJamesPureBibleSearch/programs/
-mkdir -p build-KJVLookup/Release
-cd build-KJVLookup/Release
-$QT_DIR/bin/qmake CONFIG+=release ../../KJVLookup/KJVLookup.pro
-make -j 4
-cp KJVLookup $BUILD_HOME/KJVCanOpener/app/
-ln -s $BUILD_HOME/KJVCanOpener/app/KJVLookup $INSTALL_DIR/KJVLookup
-
-
-# ---------------------
-# Install database and related pieces
+# Install databases and related pieces
 # ---------------------
 cd $BUILD_HOME/KJVCanOpener/
 cp -r $PROJECT_DIR/KingJamesPureBibleSearch/programs/KJVCanOpener/db .
@@ -188,6 +252,36 @@ cp -r $PROJECT_DIR/KingJamesPureBibleSearch/programs/KJVCanOpener/doc .
 cp -r $PROJECT_DIR/KingJamesPureBibleSearch/programs/KJVCanOpener/fonts .
 cp -r $PROJECT_DIR/KingJamesPureBibleSearch/programs/KJVCanOpener/geoip .
 cp -r $PROJECT_DIR/KingJamesPureBibleSearch/programs/KJVCanOpener/translations .
+ln -s $QT_DIR/plugins $BUILD_HOME/KJVCanOpener/plugins
+ln -s $QT_DIR/plugins/platforms $BUILD_HOME/KJVCanOpener/app/platforms
 
+cd $BUILD_HOME/KJVDataParse/
+cp -r $PROJECT_DIR/KingJamesPureBibleSearch/programs/KJVDataParse/translations .
+
+
+# ---------------------
+# Setup noVNC skeleton
+# ---------------------
+cp -r $PROJECT_DIR/KingJamesPureBibleSearch/programs/vnc/noVNC /var/www/html/
+ln -s /var/www/html/noVNC/vnc.html /var/www/html/noVNC/index.html
+
+
+# ---------------------
+# Cleanup Permissions
+# ---------------------
+chown -R $USER:$USER $BUILD_HOME
+
+
+# ---------------------
+# Cleanup Image
+# ---------------------
+#rm -rf $PROJECT_DIR/KingJamesPureBibleSearch
+apt-get clean -y
+apt-get autoclean -y
+apt-get autoremove -y
+
+# This would save ~150MB, at the expense of breaking apt-get
+# until a fresh apt-get update. Omitted for now
+#rm -r /var/lib/apt/lists/*
 
 
