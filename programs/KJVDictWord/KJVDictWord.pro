6d5bb00fc36f9106a25ea9b64bd9c813bbb6f681
Donna Whisnant <dewhisna@users.noreply.github.com>
2019-07-26 22:01:13 -0500

    Resurrect KJVDictWord.

--- programs/KJVDictWord/KJVDictWord.pro
+++ programs/KJVDictWord/KJVDictWord.pro
@@ -1,6 +1,6 @@
 ##*****************************************************************************
 ##
-## Copyright (C) 2013 Donna Whisnant, a.k.a. Dewtronics.
+## Copyright (C) 2013-2019 Donna Whisnant, a.k.a. Dewtronics.
 ## Contact: http://www.dewtronics.com/
 ##
 ## This file is part of the KJVCanOpener Application as originally written
@@ -27,9 +27,7 @@
 #
 #-------------------------------------------------
 
-QT       += core sql
-
-QT       -= gui
+QT       += core gui sql xml
 
 TARGET = KJVDictWord
 CONFIG   += console
@@ -37,11 +35,56 @@ CONFIG   -= app_bundle
 
 TEMPLATE = app
 
+DEFINES *= NO_PERSISTENT_SETTINGS
+console:DEFINES *= IS_CONSOLE_APP
+
+include(../qtiocompressor/src/qtiocompressor.pri)
+include(../grantlee/textdocument/textdocument.pri)
+
+CODECFORSRC = UTF-8
+CODECFORTR  = UTF-8
 
 SOURCES += \
 	main.cpp \
-	../KJVCanOpener/ParseSymbols.cpp
+	../KJVCanOpener/CSV.cpp \
+	../KJVCanOpener/dbDescriptors.cpp \
+	../KJVCanOpener/dbstruct.cpp \
+	../KJVCanOpener/Highlighter.cpp \
+	../KJVCanOpener/KJVSearchCriteria.cpp \
+	../KJVCanOpener/ModelRowForwardIterator.cpp \
+	../KJVCanOpener/ParseSymbols.cpp \
+	../KJVCanOpener/PersistentSettings.cpp \
+	../KJVCanOpener/PhraseEdit.cpp \
+	../KJVCanOpener/PhraseListModel.cpp \
+	../KJVCanOpener/ReadDB.cpp \
+	../KJVCanOpener/ReportError.cpp \
+	../KJVCanOpener/ScriptureDocument.cpp \
+	../KJVCanOpener/SearchCompleter.cpp \
+	../KJVCanOpener/Translator.cpp \
+	../KJVCanOpener/UserNotesDatabase.cpp \
+	../KJVCanOpener/VerseRichifier.cpp
+
 
 HEADERS  += \
-	../KJVCanOpener/ParseSymbols.h
+	../KJVCanOpener/CSV.h \
+	../KJVCanOpener/dbDescriptors.h \
+	../KJVCanOpener/dbstruct.h \
+	../KJVCanOpener/Highlighter.h \
+	../KJVCanOpener/KJVSearchCriteria.h \
+	../KJVCanOpener/ModelRowForwardIterator.h \
+	../KJVCanOpener/ParseSymbols.h \
+	../KJVCanOpener/PersistentSettings.h \
+	../KJVCanOpener/PhraseEdit.h \
+	../KJVCanOpener/PhraseListModel.h \
+	../KJVCanOpener/ReadDB.h \
+	../KJVCanOpener/ReportError.h \
+	../KJVCanOpener/ScriptureDocument.h \
+	../KJVCanOpener/SearchCompleter.h \
+	../KJVCanOpener/Translator.h \
+	../KJVCanOpener/UserNotesDatabase.h \
+	../KJVCanOpener/VerseRichifier.h
 
+
+###############################################################################
+
+message($$CONFIG$$escape_expand(\\n))
--- programs/KJVDictWord/main.cpp
+++ programs/KJVDictWord/main.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2013 Donna Whisnant, a.k.a. Dewtronics.
+** Copyright (C) 2013-2019 Donna Whisnant, a.k.a. Dewtronics.
 ** Contact: http://www.dewtronics.com/
 **
 ** This file is part of the KJVCanOpener Application as originally written
@@ -21,16 +21,21 @@
 **
 ****************************************************************************/
 
+#include "../KJVCanOpener/dbstruct.h"
+#include "../KJVCanOpener/dbDescriptors.h"
+#include "../KJVCanOpener/ReadDB.h"
 #include "../KJVCanOpener/ParseSymbols.h"
 
 #include <QCoreApplication>
 
-#include <QFile>
+#if QT_VERSION < 0x050000
+#include <QTextCodec>
+#endif
+
+#include <QDir>
+#include <QFileInfo>
 #include <QString>
 #include <QStringList>
-
-#include <QtSql>
-#include <QSqlQuery>
 
 #include <assert.h>
 #include <string>
@@ -38,148 +43,134 @@
 #include <algorithm>
 
 namespace {
-	const char *g_constrKJVDatabaseFilename = "../../KJVCanOpener/db/kjvtext.s3db";
-	const char *g_constrWeb1828DatabaseFilename = "../../KJVCanOpener/db/dct-web1828.s3db";
-}
+	//////////////////////////////////////////////////////////////////////
+	// File-scoped constants
+	//////////////////////////////////////////////////////////////////////
 
-// ============================================================================
+	const unsigned int VERSION = 10000;		// Version 1.0.0
 
-static QString deApostrHyphen(const QString &strWord);
+	const char *g_constrBibleDatabasePath = "../../KJVCanOpener/db/";
 
-static QString decompose(const QString &strWord)
-{
-	QString strDecomposed = strWord.normalized(QString::NormalizationForm_KD);
-
-	strDecomposed.replace(QChar(0x00C6), "Ae");				// U+00C6	&#198;		AE character
-	strDecomposed.replace(QChar(0x00E6), "ae");				// U+00E6	&#230;		ae character
-	strDecomposed.replace(QChar(0x0132), "IJ");				// U+0132	&#306;		IJ character
-	strDecomposed.replace(QChar(0x0133), "ij");				// U+0133	&#307;		ij character
-	strDecomposed.replace(QChar(0x0152), "Oe");				// U+0152	&#338;		OE character
-	strDecomposed.replace(QChar(0x0153), "oe");				// U+0153	&#339;		oe character
-
-	strDecomposed = deApostrHyphen(strDecomposed);
-
-	// There are two possible ways to remove accent marks:
-	//
-	//		1) strDecomposed.remove(QRegExp("[^a-zA-Z\\s]"));
-	//
-	//		2) Remove characters of class "Mark" (QChar::Mark_NonSpacing,
-	//				QChar::Mark_SpacingCombining, QChar::Mark_Enclosing),
-	//				which can be done by checking isMark()
-	//
-
-	for (int nPos = strDecomposed.size()-1; nPos >= 0; --nPos) {
-		if (strDecomposed.at(nPos).isMark()) strDecomposed.remove(nPos, 1);
-	}
-
-	return strDecomposed;
-}
-
-static QString deApostrHyphen(const QString &strWord)
-{
-	static const QString strApostropheRegExp = QChar('[') + QRegExp::escape(g_strApostrophes) + QChar(']');
-	static const QString strHyphenRegExp = QChar('[') + QRegExp::escape(g_strHyphens) + QChar(']');
-
-	QString strDecomposed = strWord;
-
-	strDecomposed.replace(QRegExp(strApostropheRegExp), "'");
-	strDecomposed.replace(QRegExp(strHyphenRegExp), "-");
-
-	return strDecomposed;
-}
+}	// namespace
 
 // ============================================================================
 
 int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
+	a.setApplicationVersion(QString("%1.%2.%3").arg(VERSION/10000).arg((VERSION/100)%100).arg(VERSION%100));
 
-	QSqlDatabase myKJVDatabase;
-	myKJVDatabase = QSqlDatabase::addDatabase("QSQLITE", "ReadKJVConnection");
-	myKJVDatabase.setDatabaseName(g_constrKJVDatabaseFilename);
-	myKJVDatabase.setConnectOptions("QSQLITE_OPEN_READONLY");
+#if QT_VERSION < 0x050000
+	QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
+#endif
 
-	QSqlDatabase myWeb1828Database;
-	myWeb1828Database = QSqlDatabase::addDatabase("QSQLITE", "ReadWeb1828Connection");
-	myWeb1828Database.setDatabaseName(g_constrWeb1828DatabaseFilename);
-	myWeb1828Database.setConnectOptions("QSQLITE_OPEN_READONLY");
+	int nBibleDescriptor = -1;
+	int nDictDescriptor = -1;
+	int nArgsFound = 0;
+	TBibleDescriptor bblDescriptor;
+	TDictionaryDescriptor dictDescriptor;
+	bool bUnknownOption = false;
+	bool bUseConcordanceList = false;
 
-	if (!myKJVDatabase.open()) {
-		std::cerr << QString("*** Failed to open Bible Database: %1\nError: %2\n\n").arg(g_constrKJVDatabaseFilename).arg(myKJVDatabase.lastError().text()).toUtf8().data();
+	for (int ndx = 1; ndx < argc; ++ndx) {
+		QString strArg = QString::fromUtf8(argv[ndx]);
+		if (!strArg.startsWith("-")) {
+			++nArgsFound;
+			if (nArgsFound == 1) {
+				nBibleDescriptor = strArg.toInt();
+			} else if (nArgsFound == 2) {
+				nDictDescriptor = strArg.toInt();
+			}
+		} else if (strArg.compare("-c") == 0) {
+			bUseConcordanceList = true;
+		} else {
+			bUnknownOption = true;
+		}
+	}
+
+	if ((nArgsFound != 2) || (bUnknownOption)) {
+		std::cerr << QString("KJVDictWord Version %1\n\n").arg(a.applicationVersion()).toUtf8().data();
+		std::cerr << QString("Usage: %1 [options] <Bible-UUID-Index> <Dictionary-UUID-Index>\n\n").arg(argv[0]).toUtf8().data();
+		std::cerr << QString("Reads the specified Bible Database and Dictionary Database and\n").toUtf8().data();
+		std::cerr << QString("    searches every word in the Bible to see if it's found in\n").toUtf8().data();
+		std::cerr << QString("    the dictionary\n").toUtf8().data();
+		std::cerr << QString("\n").toUtf8().data();
+		std::cerr << QString("Options are:\n").toUtf8().data();
+		std::cerr << QString("  -c = Use full Concordance List\n").toUtf8().data();
+		std::cerr << QString("\n").toUtf8().data();
+		std::cerr << QString("Bible-UUID-Index:\n").toUtf8().data();
+		for (unsigned int ndx = 0; ndx < bibleDescriptorCount(); ++ndx) {
+			std::cerr << QString("    %1 = %2\n").arg(ndx).arg(bibleDescriptor(static_cast<BIBLE_DESCRIPTOR_ENUM>(ndx)).m_strDBDesc).toUtf8().data();
+		}
+		std::cerr << "\n";
+		for (unsigned int ndx = 0; ndx < dictionaryDescriptorCount(); ++ndx) {
+			std::cerr << QString("    %1 = %2\n").arg(ndx).arg(dictionaryDescriptor(static_cast<DICTIONARY_DESCRIPTOR_ENUM>(ndx)).m_strDBDesc).toUtf8().data();
+		}
+		std::cerr << "\n";
 		return -1;
 	}
 
-	if (!myWeb1828Database.open()) {
-		std::cerr << QString("*** Failed to open Dictionary Database: %1\nError: %2\n\n").arg(g_constrWeb1828DatabaseFilename).arg(myWeb1828Database.lastError().text()).toUtf8().data();
-		myKJVDatabase.close();
+	if ((nBibleDescriptor >= 0) && (static_cast<unsigned int>(nBibleDescriptor) < bibleDescriptorCount())) {
+		bblDescriptor = bibleDescriptor(static_cast<BIBLE_DESCRIPTOR_ENUM>(nBibleDescriptor));
+	} else {
+		std::cerr << "Unknown Bible-UUID-Index\n";
+		return -1;
+	}
+
+	if ((nDictDescriptor >= 0) && (static_cast<unsigned int>(nDictDescriptor) < dictionaryDescriptorCount())) {
+		dictDescriptor = dictionaryDescriptor(static_cast<DICTIONARY_DESCRIPTOR_ENUM>(nDictDescriptor));
+	} else {
+		std::cerr << "Unknown Dictionary-UUID-Index\n";
+		return -1;
+	}
+
+	// ------------------------------------------------------------------------
+
+	QFileInfo fiBblDBPath(QDir(QCoreApplication::applicationDirPath()), g_constrBibleDatabasePath);
+	QFileInfo fiDictDBPath(QDir(QCoreApplication::applicationDirPath()), g_constrBibleDatabasePath);
+
+	CReadDatabase rdbMain(fiBblDBPath.absoluteFilePath(), fiDictDBPath.absoluteFilePath(), NULL);
+
+	std::cerr << QString("Reading Bible Database: %1\n").arg(bblDescriptor.m_strDBName).toUtf8().data();
+	if (!rdbMain.haveBibleDatabaseFiles(bblDescriptor)) {
+		std::cerr << QString("\n*** ERROR: Unable to locate Bible Database Files!\n").toUtf8().data();
 		return -2;
 	}
-
-	// ------------------------------------------------------------------------
-
-	QStringList lstDictWords;
-
-	QSqlQuery queryWeb1828Table(myWeb1828Database);
-
-	// Check to see if the table exists:
-	if (!queryWeb1828Table.exec("SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name='dictionary'")) {
-		std::cerr << QString("Table Lookup for \"dictioanry\" Failed!\nError: %1\n\n").arg(queryWeb1828Table.lastError().text()).toUtf8().data();
+	if (!rdbMain.ReadBibleDatabase(bblDescriptor, true)) {
+		std::cerr << QString("\n*** ERROR: Failed to Read the Bible Database!\n").toUtf8().data();
 		return -3;
 	}
-	queryWeb1828Table.next();
-	if (!queryWeb1828Table.value(0).toInt()) {
-		std::cerr << QString("Unable to find \"dictionary\" Table in database!\n\n").toUtf8().data();
-		return -4;
-	}
-	queryWeb1828Table.finish();
 
-	QSqlQuery queryWeb1828Data(myWeb1828Database);
-	queryWeb1828Data.setForwardOnly(true);
-	queryWeb1828Data.exec("SELECT id,topic,definition FROM dictionary");
-	while (queryWeb1828Data.next()) {
-		lstDictWords.append(decompose(queryWeb1828Data.value(1).toString()).toLower());
+	std::cerr << QString("Reading Dictionary Database: %1\n").arg(dictDescriptor.m_strDBName).toUtf8().data();
+	if (!rdbMain.haveDictionaryDatabaseFiles(dictDescriptor)) {
+		std::cerr << QString("\n*** ERROR: Unable to locate Dictionary Database Files!\n").toUtf8().data();
+		return -2;
 	}
-	queryWeb1828Data.finish();
+	if (!rdbMain.ReadDictionaryDatabase(dictDescriptor, false, true)) {
+		std::cerr << QString("\n*** ERROR: Failed to Read the Dictionary Database!\n").toUtf8().data();
+		return -3;
+	}
 
 	// ------------------------------------------------------------------------
 
-	QStringList lstKJVWords;
+	CBibleDatabasePtr pBibleDatabase = TBibleDatabaseList::instance()->mainBibleDatabase();
+	CDictionaryDatabasePtr pDictDatabase = TDictionaryDatabaseList::instance()->mainDictionaryDatabase();
 
-	QSqlQuery queryKJVTable(myKJVDatabase);
+	std::cout << "Bible-Word,Dictionary-Word\n";
+	for (int ndx = 0; ndx < (bUseConcordanceList ?	pBibleDatabase->concordanceWordList().size() :
+													pBibleDatabase->lstWordList().size()); ++ndx) {
+		QString strWord = bUseConcordanceList ? pBibleDatabase->concordanceWordList().at(ndx).decomposedWord() :
+												pBibleDatabase->lstWordList().at(ndx);
 
-	// Check to see if the table exists:
-	if (!queryKJVTable.exec("SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name='WORDS'")) {
-		std::cerr << QString("Table Lookup for \"WORDS\" Failed!\nError: %1\n\n").arg(queryKJVTable.lastError().text()).toUtf8().data();
-		return -5;
-	}
-	queryKJVTable.next();
-	if (!queryKJVTable.value(0).toInt()) {
-		std::cerr << QString("Unable to find \"WORDS\" Table in database!\n\n").toUtf8().data();
-		return -6;
-	}
-	queryKJVTable.finish();
-
-	QSqlQuery queryKJVData(myKJVDatabase);
-	queryKJVData.setForwardOnly(true);
-	queryKJVData.exec("SELECT * FROM WORDS");
-	while (queryKJVData.next()) {
-		lstKJVWords.append(decompose(queryKJVData.value(1).toString()).toLower());
-	}
-	queryKJVData.finish();
-
-	std::sort(lstKJVWords.begin(), lstKJVWords.end());
-
-	std::cout << "KJVWord,DictWord\n";
-	for (int ndx = 0; ndx < lstKJVWords.size(); ++ndx) {
-		QString strWord = lstKJVWords.at(ndx);
-		if (lstDictWords.contains(strWord)) {
+		if (pDictDatabase->wordExists(strWord)) {
 			std::cout << strWord.toUtf8().data() << "," << strWord.toUtf8().data() << "\n";
 		} else {
 			std::cout << strWord.toUtf8().data() << ",\n";
 		}
 	}
 
-	myWeb1828Database.close();
-	myKJVDatabase.close();
+	// ------------------------------------------------------------------------
+
+//	return a.exec();
 	return 0;
 }
