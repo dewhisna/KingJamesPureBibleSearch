dewhisna/KingJamesPureBibleSearch

commit 318a4e241717381400a0ace455412ab28c30e766

Remove goofy uint32_t defines and add 'std=c++11' setting. With the lambdas and such, a minimum of C++11 is needed.

programs/KJVCanOpener/dbstruct.h
@@ -49,10 +49,6 @@
 
 #include <assert.h>
 
-#ifndef uint32_t
-#define uint32_t unsigned int
-#endif
-
 #ifndef _countof
 #define _countof(x) (sizeof(x)/sizeof(x[0]))
 #endif

programs/KJVCanOpener/KJVCanOpener.pro
@@ -27,6 +27,12 @@
 #
 #-------------------------------------------------
 
+greaterThan(QT_MAJOR_VERSION,4) {
+	CONFIG *= c++11
+} else {
+	QMAKE_CXXFLAGS *= -std=c++11
+}
+
 QT       += core gui xml
 !emscripten {
 	QT += sql

programs/KJVCanOpener/kjvdatagen/Makefile
@@ -1,6 +1,6 @@
 CXX = g++
 LD = g++
-CXXFLAGS = -Wall -W -pedantic -g -ggdb -I/cygdrive/c/Boost/boost_1_51_0 -c -o
+CXXFLAGS = -std=c++11 -Wall -W -pedantic -g -ggdb -I/cygdrive/c/Boost/boost_1_51_0 -c -o
 LDFLAGS = -o $(EXEC)
 EXEC = kjvdatagen
 OBJECTS = kjvdatagen.o

programs/KJVCanOpener/kjvdatagen/kjvdatagen.cpp
@@ -127,10 +127,6 @@
 #define stricmp strcasecmp
 #endif
 
-#ifndef uint32_t
-#define uint32_t unsigned int
-#endif
-
 // PILCROW_SUMMARY : Set to 1 to output Pilcrow report summary to <stderr> or 0 to suppress
 #define PILCROW_SUMMARY 0
 // OUTPUT_HEBREW_PS119 : Set to 1 to output Hebrew characters in addition to English for Psalm119 in Rich text

commit 6f519780b24deb476a09dc514aaa0c91b27631a1

Fix the WebChannel signal/slot data typing so that connections work again. This fixes the stuff broken by 318a4e241717381400a0ace455412ab28c30e766, when we removed the ridiculous define of uint32_t to 'unsigned int'. Unfortunately, unlike 'unsigned int', 'uint32_t' doesn't have a Qt defined meta-type, meaning signals and slots didn't work until we defined it. This change makes the entire connection mechanism consistent, converting all CRelIndex types to their proper uint32_t instead of being 'unsigned int' at random places.  And this change makes the chapter indexes the proper 'unsigned int' type instead of (signed) 'int', since the book, chapter, verse, word indices from CRelIndex are unsigned.  Though it currently leaves the chapters as 'unsigned int' instead of 'uint32_t', since that isn't completely consistent throughout the code and is 'unsigned int' in more places than 'uint32_t'.

programs/KJVCanOpener/DelayedExecutionTimer.cpp
@@ -49,7 +49,7 @@ void DelayedExecutionTimer::timeout()
 	emit triggered();
 	emit triggered(m_prefix % m_lastString % m_postfix);
 	emit triggered(m_lastInt);
-	emit triggered(static_cast<unsigned int>(m_lastInt));
+	emit triggered(static_cast<uint32_t>(m_lastInt));
 	emit triggered(m_lastRelIndex);
 	emit triggered(m_lastPhraseTag);
 	emit triggered(m_lastPassageTag);
@@ -91,7 +91,7 @@ void DelayedExecutionTimer::trigger(int i)
 	trigger();
 }
 
-void DelayedExecutionTimer::trigger(unsigned int ui)
+void DelayedExecutionTimer::trigger(uint32_t ui)
 {
 	m_lastInt = static_cast<int>(ui);
 }

programs/KJVCanOpener/DelayedExecutionTimer.h
@@ -83,7 +83,7 @@ signals:
 	void triggered();
 	void triggered(const QString &string);
 	void triggered(int i);
-	void triggered(unsigned int ui);
+	void triggered(uint32_t ui);
 	void triggered(const CRelIndex &ndx);
 	void triggered(const TPhraseTag &tag);
 	void triggered(const TPassageTag &tag);
@@ -93,7 +93,7 @@ public slots:
 	void trigger();
 	void trigger(const QString &string);
 	void trigger(int i);
-	void trigger(unsigned int ui);
+	void trigger(uint32_t ui);
 	void trigger(const CRelIndex &ndx);
 	void trigger(const TPhraseTag &tag);
 	void trigger(const TPassageTag &tag);

programs/KJVCanOpener/dbstruct.h
@@ -184,6 +184,7 @@ inline QDataStream& operator>>(QDataStream &in, CRelIndex &ndx) {
 	return in;
 }
 Q_DECLARE_METATYPE(CRelIndex)
+Q_DECLARE_METATYPE(uint32_t)
 
 // ============================================================================
 

programs/KJVCanOpener/myApplication.cpp
@@ -1499,6 +1499,7 @@ int CMyApplication::execute(bool bBuildDB)
 	qRegisterMetaType<CBibleDatabasePtr>("CBibleDatabasePtr");						// Needed to do queued connection on WebChannel threads
 	qRegisterMetaType<CDictionaryDatabasePtr>("CDictionaryDatabasePtr");			// Needed to do queued connection on WebChannel threads
 	qRegisterMetaType<CUserNotesDatabasePtr>("CUserNotesDatabasePtr");				// Needed to do queued connection on WebChannel threads
+	qRegisterMetaType<uint32_t>("uint32_t");										// Needed to do queued connection on WebChannel threads
 
 	// Setup Text-To-Speech:
 #ifdef USING_QT_SPEECH

programs/qwebchannel/webChannelObjects.cpp
@@ -147,7 +147,7 @@ void CWebChannelObjects::gotoIndex(uint32_t ndxRel, int nMoveMode, const QString
 	CWebChannelThreadController::instance()->gotoIndex(this, ndxRel, nMoveMode, strParam);
 }
 
-void CWebChannelObjects::gotoChapter(int nChp, const QString &strParam)
+void CWebChannelObjects::gotoChapter(unsigned int nChp, const QString &strParam)
 {
 	CWebChannelThreadController::instance()->gotoChapter(this, nChp, strParam);
 }

programs/qwebchannel/webChannelObjects.h
@@ -84,7 +84,7 @@ public slots:
 	void resolvePassageReference(const QString &strPassageReference);
 
 	void gotoIndex(uint32_t ndxRel, int nMoveMode, const QString &strParam);			// Passage to navigate Scripture Browser to relative to nMoveMode. strParam is misc parameter sent back to javascript via scriptureBrowserRender()
-	void gotoChapter(int nChp, const QString &strParam);	// Passage to navigate Scripture Browser to by chapter index
+	void gotoChapter(unsigned int nChp, const QString &strParam);	// Passage to navigate Scripture Browser to by chapter index
 
 protected slots:
 	// ------------ Threaded Results Slots:
@@ -113,7 +113,7 @@ signals:
 
 	void resolvedPassageReference(uint32_t ndxRel, uint32_t nWordCount);				// Triggered after call to resolvePassageReference with the TPhraseTag equivalent of the passage
 
-	void scriptureBrowserRender(int nChp, uint32_t ndxRel, const QString &strHtmlScripture, const QString &strParam);		// Triggered by scripture browser navigation to display rendered text
+	void scriptureBrowserRender(unsigned int nChp, uint32_t ndxRel, const QString &strHtmlScripture, const QString &strParam);		// Triggered by scripture browser navigation to display rendered text
 	void setBibleAudioURLs(const QString &strURLListJson);								// Triggered by scripture browser navigation to set a list of valid URLs for Bible Audio
 
 private:

programs/qwebchannel/webChannelSearchResults.cpp
@@ -88,7 +88,7 @@ void CWebChannelSearchResults::initialize(CBibleDatabasePtr pBibleDatabase, CUse
 	}
 	if (m_pRetriggerGetSearchResultDetails.isNull()) {
 		m_pRetriggerGetSearchResultDetails = new DelayedExecutionTimer(-1, RETRIGGER_TIME, this);
-		connect(m_pRetriggerGetSearchResultDetails.data(), SIGNAL(triggered(unsigned int)), this, SLOT(getSearchResultDetails(unsigned int)));
+		connect(m_pRetriggerGetSearchResultDetails.data(), SIGNAL(triggered(uint32_t)), this, SLOT(getSearchResultDetails(uint32_t)));
 	}
 	if (m_pRetriggerGotoIndex.isNull()) {
 		m_pRetriggerGotoIndex = new DelayedExecutionTimer(-1, RETRIGGER_TIME, this);
@@ -469,7 +469,7 @@ void CWebChannelSearchResults::getMoreSearchResults()
 	}
 }
 
-void CWebChannelSearchResults::getSearchResultDetails(unsigned int ndxLogical)
+void CWebChannelSearchResults::getSearchResultDetails(uint32_t ndxLogical)
 {
 	// If we fell asleep before this call, restart the search results since the
 	//		previous result will have been tossed:
@@ -508,7 +508,7 @@ void CWebChannelSearchResults::en_retriggerGotoIndex(const QString &strData)
 	gotoIndex(strNdxRel.toUInt(), strMoveMode.toInt(), strParam);
 }
 
-void CWebChannelSearchResults::gotoIndex(unsigned int ndxRel, int nMoveMode, const QString &strParam)
+void CWebChannelSearchResults::gotoIndex(uint32_t ndxRel, int nMoveMode, const QString &strParam)
 {
 	// If we fell asleep before this call, restart the search results since the
 	//		previous result will have been tossed:
@@ -579,7 +579,7 @@ void CWebChannelSearchResults::gotoIndex(unsigned int ndxRel, int nMoveMode, con
 	m_pPhraseNavigator->clearDocument();			// Free-up memory for other clients
 }
 
-void CWebChannelSearchResults::gotoChapter(int nChp, const QString &strParam)
+void CWebChannelSearchResults::gotoChapter(unsigned int nChp, const QString &strParam)
 {
 	CRelIndex ndx = m_pBibleDatabase->calcRelIndex(0, 0, nChp, 0, 0);
 	if (ndx.isSet()) {
@@ -712,11 +712,11 @@ CWebChannelSearchResults *CWebChannelThreadController::createWebChannelSearchRes
 		connect(pSearchResults, SIGNAL(setAutoCompleter(const QString &, const QString &)), pChannel, SIGNAL(setAutoCompleter(const QString &, const QString &)));
 		connect(pSearchResults, SIGNAL(updatePhrase(const QString &, const QString &)), pChannel, SIGNAL(updatePhrase(const QString &, const QString &)));
 
-		connect(pSearchResults, SIGNAL(searchResultsDetails(unsigned int, const QString &)), pChannel, SIGNAL(searchResultsDetails(unsigned int, const QString &)));
+		connect(pSearchResults, SIGNAL(searchResultsDetails(uint32_t, const QString &)), pChannel, SIGNAL(searchResultsDetails(uint32_t, const QString &)));
 
-		connect(pSearchResults, SIGNAL(resolvedPassageReference(unsigned int, unsigned int)), pChannel, SIGNAL(resolvedPassageReference(unsigned int, unsigned int)));
+		connect(pSearchResults, SIGNAL(resolvedPassageReference(uint32_t, uint32_t)), pChannel, SIGNAL(resolvedPassageReference(uint32_t, uint32_t)));
 
-		connect(pSearchResults, SIGNAL(scriptureBrowserRender(int, unsigned int, const QString &, const QString &)), pChannel, SIGNAL(scriptureBrowserRender(int, unsigned int, const QString &, const QString &)));
+		connect(pSearchResults, SIGNAL(scriptureBrowserRender(unsigned int, uint32_t, const QString &, const QString &)), pChannel, SIGNAL(scriptureBrowserRender(unsigned int, uint32_t, const QString &, const QString &)));
 		connect(pSearchResults, SIGNAL(setBibleAudioURLs(const QString &)), pChannel, SIGNAL(setBibleAudioURLs(const QString &)));
 	}
 
@@ -779,9 +779,9 @@ void CWebChannelThreadController::destroyWebChannelSearchResults(CWebChannelObje
 	disconnect(pSearchResults, SIGNAL(setAutoCorrectText(const QString &, const QString &)), pChannel, SIGNAL(setAutoCorrectText(const QString &, const QString &)));
 	disconnect(pSearchResults, SIGNAL(setAutoCompleter(const QString &, const QString &)), pChannel, SIGNAL(setAutoCompleter(const QString &, const QString &)));
 	disconnect(pSearchResults, SIGNAL(updatePhrase(const QString &, const QString &)), pChannel, SIGNAL(updatePhrase(const QString &, const QString &)));
-	disconnect(pSearchResults, SIGNAL(searchResultsDetails(unsigned int, const QString &)), pChannel, SIGNAL(searchResultsDetails(unsigned int, const QString &)));
-	disconnect(pSearchResults, SIGNAL(resolvedPassageReference(unsigned int, unsigned int)), pChannel, SIGNAL(resolvedPassageReference(unsigned int, unsigned int)));
-	disconnect(pSearchResults, SIGNAL(scriptureBrowserRender(int, unsigned int, const QString &, const QString &)), pChannel, SIGNAL(scriptureBrowserRender(int, unsigned int, const QString &, const QString &)));
+	disconnect(pSearchResults, SIGNAL(searchResultsDetails(uint32_t, const QString &)), pChannel, SIGNAL(searchResultsDetails(uint32_t, const QString &)));
+	disconnect(pSearchResults, SIGNAL(resolvedPassageReference(uint32_t, uint32_t)), pChannel, SIGNAL(resolvedPassageReference(uint32_t, uint32_t)));
+	disconnect(pSearchResults, SIGNAL(scriptureBrowserRender(unsigned int, uint32_t, const QString &, const QString &)), pChannel, SIGNAL(scriptureBrowserRender(unsigned int, uint32_t, const QString &, const QString &)));
 	disconnect(pSearchResults, SIGNAL(setBibleAudioURLs(const QString &)), pChannel, SIGNAL(setBibleAudioURLs(const QString &)));
 
 	pSearchResults->deleteLater();
@@ -898,7 +898,7 @@ void CWebChannelThreadController::calcUpdatedPhrase(CWebChannelObjects *pChannel
 	}
 }
 
-void CWebChannelThreadController::getSearchResultDetails(CWebChannelObjects *pChannel, unsigned int ndxLogical)
+void CWebChannelThreadController::getSearchResultDetails(CWebChannelObjects *pChannel, uint32_t ndxLogical)
 {
 	assert(pChannel != NULL);
 	CWebChannelSearchResults *pSearchResults = m_mapSearchResults.value(pChannel, NULL);
@@ -906,7 +906,7 @@ void CWebChannelThreadController::getSearchResultDetails(CWebChannelObjects *pCh
 		bool bSuccess = QMetaObject::invokeMethod(pSearchResults,
 													"getSearchResultDetails",
 													Qt::QueuedConnection,
-													Q_ARG(unsigned int, ndxLogical));
+													Q_ARG(uint32_t, ndxLogical));
 		assert(bSuccess);
 	}
 }
@@ -924,7 +924,7 @@ void CWebChannelThreadController::resolvePassageReference(CWebChannelObjects *pC
 	}
 }
 
-void CWebChannelThreadController::gotoIndex(CWebChannelObjects *pChannel, unsigned int ndxRel, int nMoveMode, const QString &strParam)
+void CWebChannelThreadController::gotoIndex(CWebChannelObjects *pChannel, uint32_t ndxRel, int nMoveMode, const QString &strParam)
 {
 	assert(pChannel != NULL);
 	CWebChannelSearchResults *pSearchResults = m_mapSearchResults.value(pChannel, NULL);
@@ -932,14 +932,14 @@ void CWebChannelThreadController::gotoIndex(CWebChannelObjects *pChannel, unsign
 		bool bSuccess = QMetaObject::invokeMethod(pSearchResults,
 													"gotoIndex",
 													Qt::QueuedConnection,
-													Q_ARG(unsigned int, ndxRel),
+													Q_ARG(uint32_t, ndxRel),
 													Q_ARG(int, nMoveMode),
 													Q_ARG(const QString &, strParam));
 		assert(bSuccess);
 	}
 }
 
-void CWebChannelThreadController::gotoChapter(CWebChannelObjects *pChannel, int nChp, const QString &strParam)
+void CWebChannelThreadController::gotoChapter(CWebChannelObjects *pChannel, unsigned int nChp, const QString &strParam)
 {
 	assert(pChannel != NULL);
 	CWebChannelSearchResults *pSearchResults = m_mapSearchResults.value(pChannel, NULL);
@@ -947,7 +947,7 @@ void CWebChannelThreadController::gotoChapter(CWebChannelObjects *pChannel, int 
 		bool bSuccess = QMetaObject::invokeMethod(pSearchResults,
 													"gotoChapter",
 													Qt::QueuedConnection,
-													Q_ARG(int, nChp),
+													Q_ARG(unsigned int, nChp),
 													Q_ARG(const QString &, strParam));
 		assert(bSuccess);
 	}

programs/qwebchannel/webChannelSearchResults.h
@@ -70,12 +70,12 @@ public slots:
 	void autoCorrect(const QString &strElementID, const QString &strPhrase, int nCursorPos, const QString &strLastPhrase, int nLastCursorPos);			// Returns HTML Auto-Correction string for passed phrase and triggers autoCompleter list
 	void calcUpdatedPhrase(const QString &strElementID, const QString &strPhrase, const QString &strAutoCompleter, int nCursorPos);		// Runs Phrase Parser and determines current subphrase.  Replaces that subphrase with passed strAutoCompleter value
 
-	void getSearchResultDetails(unsigned int ndxLogical);	// Requests data for ToolTip (i.e. search results detail) for the specified logical index
+	void getSearchResultDetails(uint32_t ndxLogical);	// Requests data for ToolTip (i.e. search results detail) for the specified logical index
 
 	void resolvePassageReference(const QString &strPassageReference);
 
-	void gotoIndex(unsigned int ndxRel, int nMoveMode, const QString &strParam);		// Passage to navigate Scripture Browser to relative to nMoveMode. strParam is misc parameter sent back to javascript via scriptureBrowserRender()
-	void gotoChapter(int nChp, const QString &strParam);	// Passage to navigate Scripture Browser to by chapter index
+	void gotoIndex(uint32_t ndxRel, int nMoveMode, const QString &strParam);		// Passage to navigate Scripture Browser to relative to nMoveMode. strParam is misc parameter sent back to javascript via scriptureBrowserRender()
+	void gotoChapter(unsigned int nChp, const QString &strParam);	// Passage to navigate Scripture Browser to by chapter index
 
 signals:
 	void bibleSelected(bool bSuccess, const QString &strUUID, const QString &strJsonBkChpStruct);		// Generated after selectBible() call to indicate success/fail and to provide the book/chapter layout for navigation (empty if failure)
@@ -87,11 +87,11 @@ signals:
 	void setAutoCompleter(const QString &strElementID, const QString &strWordList);		// Triggered after call to autoCorrect() will return a list of words separated by ";"
 	void updatePhrase(const QString &strElementID, const QString &strNewPhrase);		// Triggered after call to calcUpdatedPhrase() to return new phrase with subphrase substituted
 
-	void searchResultsDetails(unsigned int ndxLogical, const QString &strDetails);		// Triggered after call to getSearchResultDetails() to return search details i.e. tool tip for specified index
+	void searchResultsDetails(uint32_t ndxLogical, const QString &strDetails);		// Triggered after call to getSearchResultDetails() to return search details i.e. tool tip for specified index
 
-	void resolvedPassageReference(unsigned int ndxRel, unsigned int nWordCount);		// Triggered after call to resolvePassageReference with the TPhraseTag equivalent of the passage
+	void resolvedPassageReference(uint32_t ndxRel, uint32_t nWordCount);		// Triggered after call to resolvePassageReference with the TPhraseTag equivalent of the passage
 
-	void scriptureBrowserRender(int nChp, unsigned int ndxRel, const QString &strHtmlScripture, const QString &strParam);		// Triggered by scripture browser navigation to display rendered text
+	void scriptureBrowserRender(unsigned int nChp, uint32_t ndxRel, const QString &strHtmlScripture, const QString &strParam);		// Triggered by scripture browser navigation to display rendered text
 	void setBibleAudioURLs(const QString &strURLListJson);								// Triggered by scripture browser navigation to set a list of valid URLs for Bible Audio
 
 	void idleStateChanged(bool bIsIdle);					// Triggered when connection either goes idle or wakes up
@@ -191,12 +191,12 @@ public:
 	void autoCorrect(CWebChannelObjects *pChannel, const QString &strElementID, const QString &strPhrase, int nCursorPos, const QString &strLastPhrase, int nLastCursorPos);			// Returns HTML Auto-Correction string for passed phrase and triggers autoCompleter list
 	void calcUpdatedPhrase(CWebChannelObjects *pChannel, const QString &strElementID, const QString &strPhrase, const QString &strAutoCompleter, int nCursorPos);		// Runs Phrase Parser and determines current subphrase.  Replaces that subphrase with passed strAutoCompleter value
 
-	void getSearchResultDetails(CWebChannelObjects *pChannel, unsigned int ndxLogical);		// Requests data for ToolTip (i.e. search results detail) for the specified logical index
+	void getSearchResultDetails(CWebChannelObjects *pChannel, uint32_t ndxLogical);		// Requests data for ToolTip (i.e. search results detail) for the specified logical index
 
 	void resolvePassageReference(CWebChannelObjects *pChannel, const QString &strPassageReference);
 
-	void gotoIndex(CWebChannelObjects *pChannel, unsigned int ndxRel, int nMoveMode, const QString &strParam);			// Passage to navigate Scripture Browser to relative to nMoveMode. strParam is misc parameter sent back to javascript via scriptureBrowserRender()
-	void gotoChapter(CWebChannelObjects *pChannel, int nChp, const QString &strParam);	// Passage to navigate Scripture Browser to by chapter index
+	void gotoIndex(CWebChannelObjects *pChannel, uint32_t ndxRel, int nMoveMode, const QString &strParam);			// Passage to navigate Scripture Browser to relative to nMoveMode. strParam is misc parameter sent back to javascript via scriptureBrowserRender()
+	void gotoChapter(CWebChannelObjects *pChannel, unsigned int nChp, const QString &strParam);	// Passage to navigate Scripture Browser to by chapter index
 
 private:
 	CWebChannelSearchResults *createWebChannelSearchResults(CWebChannelObjects *pChannel,  CBibleDatabasePtr pBibleDatabase, CUserNotesDatabasePtr pUserNotesDatabase);