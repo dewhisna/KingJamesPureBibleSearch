dewhisna/KingJamesPureBibleSearch

commit 7d6cb5d790b73af7535649b97e9e83c03a4f08a7

Normalize signal/slot connect/disconnect/invokeMethod signatures.

programs/KJVCanOpener/qwebchannel/webChannelSearchResults.cpp
@@ -98,7 +98,7 @@ void CWebChannelSearchResults::initialize(CBibleDatabasePtr pBibleDatabase, CUse
 	}
 	if (m_pRetriggerGotoIndex.isNull()) {
 		m_pRetriggerGotoIndex = new DelayedExecutionTimer(-1, RETRIGGER_TIME, this);
-		connect(m_pRetriggerGotoIndex.data(), SIGNAL(triggered(const QString &)), this, SLOT(en_retriggerGotoIndex(const QString &)));
+		connect(m_pRetriggerGotoIndex.data(), SIGNAL(triggered(QString)), this, SLOT(en_retriggerGotoIndex(QString)));
 	}
 
 	m_strLastPhrases.clear();
@@ -724,21 +724,21 @@ CWebChannelSearchResults *CWebChannelThreadController::createWebChannelSearchRes
 		connect(pSearchResults, SIGNAL(idleStateChanged(bool)), pChannel, SLOT(en_idleStateChanged(bool)));
 		connect(pSearchResults, SIGNAL(killWebChannel()), pChannel, SLOT(en_killWebChannel()));
 
-		connect(pSearchResults, SIGNAL(bibleSelected(bool, const QString &, const QString &)), pChannel, SIGNAL(bibleSelected(bool, const QString &, const QString &)));
-		connect(pSearchResults, SIGNAL(searchWithinModelChanged(const QString &,int)), pChannel, SIGNAL(searchWithinModelChanged(const QString &, int)));
+		connect(pSearchResults, SIGNAL(bibleSelected(bool,QString,QString)), pChannel, SIGNAL(bibleSelected(bool,QString,QString)));
+		connect(pSearchResults, SIGNAL(searchWithinModelChanged(QString,int)), pChannel, SIGNAL(searchWithinModelChanged(QString,int)));
 
-		connect(pSearchResults, SIGNAL(searchResultsChanged(const QString &, const QString &, const QString &)), pChannel, SIGNAL(searchResultsChanged(const QString &, const QString &, const QString &)));
-		connect(pSearchResults, SIGNAL(searchResultsAppend(const QString &, bool)), pChannel, SIGNAL(searchResultsAppend(const QString &, bool)));
-		connect(pSearchResults, SIGNAL(setAutoCorrectText(const QString &, const QString &)), pChannel, SIGNAL(setAutoCorrectText(const QString &, const QString &)));
-		connect(pSearchResults, SIGNAL(setAutoCompleter(const QString &, const QString &)), pChannel, SIGNAL(setAutoCompleter(const QString &, const QString &)));
-		connect(pSearchResults, SIGNAL(updatePhrase(const QString &, const QString &)), pChannel, SIGNAL(updatePhrase(const QString &, const QString &)));
+		connect(pSearchResults, SIGNAL(searchResultsChanged(QString,QString,QString)), pChannel, SIGNAL(searchResultsChanged(QString,QString,QString)));
+		connect(pSearchResults, SIGNAL(searchResultsAppend(QString,bool)), pChannel, SIGNAL(searchResultsAppend(QString,bool)));
+		connect(pSearchResults, SIGNAL(setAutoCorrectText(QString,QString)), pChannel, SIGNAL(setAutoCorrectText(QString,QString)));
+		connect(pSearchResults, SIGNAL(setAutoCompleter(QString,QString)), pChannel, SIGNAL(setAutoCompleter(QString,QString)));
+		connect(pSearchResults, SIGNAL(updatePhrase(QString,QString)), pChannel, SIGNAL(updatePhrase(QString,QString)));
 
-		connect(pSearchResults, SIGNAL(searchResultsDetails(uint32_t, const QString &)), pChannel, SIGNAL(searchResultsDetails(uint32_t, const QString &)));
+		connect(pSearchResults, SIGNAL(searchResultsDetails(uint32_t,QString)), pChannel, SIGNAL(searchResultsDetails(uint32_t,QString)));
 
-		connect(pSearchResults, SIGNAL(resolvedPassageReference(uint32_t, uint32_t)), pChannel, SIGNAL(resolvedPassageReference(uint32_t, uint32_t)));
+		connect(pSearchResults, SIGNAL(resolvedPassageReference(uint32_t,uint32_t)), pChannel, SIGNAL(resolvedPassageReference(uint32_t,uint32_t)));
 
-		connect(pSearchResults, SIGNAL(scriptureBrowserRender(unsigned int, uint32_t, const QString &, const QString &)), pChannel, SIGNAL(scriptureBrowserRender(unsigned int, uint32_t, const QString &, const QString &)));
-		connect(pSearchResults, SIGNAL(setBibleAudioURLs(const QString &)), pChannel, SIGNAL(setBibleAudioURLs(const QString &)));
+		connect(pSearchResults, SIGNAL(scriptureBrowserRender(uint,uint32_t,QString,QString)), pChannel, SIGNAL(scriptureBrowserRender(uint,uint32_t,QString,QString)));
+		connect(pSearchResults, SIGNAL(setBibleAudioURLs(QString)), pChannel, SIGNAL(setBibleAudioURLs(QString&)));
 	}
 
 	// Initialize after connects in case pSearchResults wants to emit any signals during init:
@@ -793,17 +793,17 @@ void CWebChannelThreadController::destroyWebChannelSearchResults(CWebChannelObje
 	//		all signals from CWebChannelSearchResults and CWebChannelObjects:
 	disconnect(pSearchResults, SIGNAL(idleStateChanged(bool)), pChannel, SLOT(en_idleStateChanged(bool)));
 	disconnect(pSearchResults, SIGNAL(killWebChannel()), pChannel, SLOT(en_killWebChannel()));
-	disconnect(pSearchResults, SIGNAL(bibleSelected(bool, const QString &, const QString &)), pChannel, SIGNAL(bibleSelected(bool, const QString &, const QString &)));
-	disconnect(pSearchResults, SIGNAL(searchWithinModelChanged(const QString &,int)), pChannel, SIGNAL(searchWithinModelChanged(const QString &, int)));
-	disconnect(pSearchResults, SIGNAL(searchResultsChanged(const QString &, const QString &, const QString &)), pChannel, SIGNAL(searchResultsChanged(const QString &, const QString &, const QString &)));
-	disconnect(pSearchResults, SIGNAL(searchResultsAppend(const QString &, bool)), pChannel, SIGNAL(searchResultsAppend(const QString &, bool)));
-	disconnect(pSearchResults, SIGNAL(setAutoCorrectText(const QString &, const QString &)), pChannel, SIGNAL(setAutoCorrectText(const QString &, const QString &)));
-	disconnect(pSearchResults, SIGNAL(setAutoCompleter(const QString &, const QString &)), pChannel, SIGNAL(setAutoCompleter(const QString &, const QString &)));
-	disconnect(pSearchResults, SIGNAL(updatePhrase(const QString &, const QString &)), pChannel, SIGNAL(updatePhrase(const QString &, const QString &)));
-	disconnect(pSearchResults, SIGNAL(searchResultsDetails(uint32_t, const QString &)), pChannel, SIGNAL(searchResultsDetails(uint32_t, const QString &)));
-	disconnect(pSearchResults, SIGNAL(resolvedPassageReference(uint32_t, uint32_t)), pChannel, SIGNAL(resolvedPassageReference(uint32_t, uint32_t)));
-	disconnect(pSearchResults, SIGNAL(scriptureBrowserRender(unsigned int, uint32_t, const QString &, const QString &)), pChannel, SIGNAL(scriptureBrowserRender(unsigned int, uint32_t, const QString &, const QString &)));
-	disconnect(pSearchResults, SIGNAL(setBibleAudioURLs(const QString &)), pChannel, SIGNAL(setBibleAudioURLs(const QString &)));
+	disconnect(pSearchResults, SIGNAL(bibleSelected(bool,QString,QString)), pChannel, SIGNAL(bibleSelected(bool,QString,QString)));
+	disconnect(pSearchResults, SIGNAL(searchWithinModelChanged(QString,int)), pChannel, SIGNAL(searchWithinModelChanged(QString,int)));
+	disconnect(pSearchResults, SIGNAL(searchResultsChanged(QString,QString,QString)), pChannel, SIGNAL(searchResultsChanged(QString,QString,QString)));
+	disconnect(pSearchResults, SIGNAL(searchResultsAppend(QString,bool)), pChannel, SIGNAL(searchResultsAppend(QString,bool)));
+	disconnect(pSearchResults, SIGNAL(setAutoCorrectText(QString,QString)), pChannel, SIGNAL(setAutoCorrectText(QString,QString)));
+	disconnect(pSearchResults, SIGNAL(setAutoCompleter(QString,QString)), pChannel, SIGNAL(setAutoCompleter(QString,QString)));
+	disconnect(pSearchResults, SIGNAL(updatePhrase(QString,QString)), pChannel, SIGNAL(updatePhrase(QString,QString)));
+	disconnect(pSearchResults, SIGNAL(searchResultsDetails(uint32_t,QString)), pChannel, SIGNAL(searchResultsDetails(uint32_t,QString)));
+	disconnect(pSearchResults, SIGNAL(resolvedPassageReference(uint32_t,uint32_t)), pChannel, SIGNAL(resolvedPassageReference(uint32_t,uint32_t)));
+	disconnect(pSearchResults, SIGNAL(scriptureBrowserRender(uint,uint32_t,QString,QString)), pChannel, SIGNAL(scriptureBrowserRender(uint,uint32_t,QString,QString)));
+	disconnect(pSearchResults, SIGNAL(setBibleAudioURLs(QString)), pChannel, SIGNAL(setBibleAudioURLs(QString)));
 
 	pSearchResults->deleteLater();
 }
@@ -831,7 +831,7 @@ bool CWebChannelThreadController::selectBible(CWebChannelObjects *pChannel, cons
 		bSuccess = QMetaObject::invokeMethod(pChannel,
 									"bibleList",
 									Qt::DirectConnection,		// This should make sure the list fires before the default selection propagates
-									Q_ARG(const QString &, TBibleDatabaseList::instance()->availableBibleDatabasesAsJson()));
+									Q_ARG(QString, TBibleDatabaseList::instance()->availableBibleDatabasesAsJson()));
 		Q_ASSERT(bSuccess);
 	}
 
@@ -851,8 +851,8 @@ bool CWebChannelThreadController::selectBible(CWebChannelObjects *pChannel, cons
 												"bibleSelected",
 												Qt::QueuedConnection,
 												Q_ARG(bool, false),
-												Q_ARG(const QString &, QString()),
-												Q_ARG(const QString &, QString()));
+												Q_ARG(QString, QString()),
+												Q_ARG(QString, QString()));
 		Q_ASSERT(bSuccess);
 	}
 
@@ -867,8 +867,8 @@ void CWebChannelThreadController::setSearchPhrases(CWebChannelObjects *pChannel,
 		bool bSuccess = QMetaObject::invokeMethod(pSearchResults,
 													"setSearchPhrases",
 													Qt::QueuedConnection,
-													Q_ARG(const QString &, strPhrases),
-													Q_ARG(const QString &, strSearchWithin),
+													Q_ARG(QString, strPhrases),
+													Q_ARG(QString, strSearchWithin),
 													Q_ARG(int, nSearchScope));
 		Q_ASSERT(bSuccess);
 	}
@@ -894,10 +894,10 @@ void CWebChannelThreadController::autoCorrect(CWebChannelObjects *pChannel, cons
 		bool bSuccess = QMetaObject::invokeMethod(pSearchResults,
 													"autoCorrect",
 													Qt::QueuedConnection,
-													Q_ARG(const QString &, strElementID),
-													Q_ARG(const QString &, strPhrase),
+													Q_ARG(QString, strElementID),
+													Q_ARG(QString, strPhrase),
 													Q_ARG(int, nCursorPos),
-													Q_ARG(const QString &, strLastPhrase),
+													Q_ARG(QString, strLastPhrase),
 													Q_ARG(int,  nLastCursorPos));
 		Q_ASSERT(bSuccess);
 	}
@@ -911,9 +911,9 @@ void CWebChannelThreadController::calcUpdatedPhrase(CWebChannelObjects *pChannel
 		bool bSuccess = QMetaObject::invokeMethod(pSearchResults,
 													"calcUpdatedPhrase",
 													Qt::QueuedConnection,
-													Q_ARG(const QString &, strElementID),
-													Q_ARG(const QString &, strPhrase),
-													Q_ARG(const QString &, strAutoCompleter),
+													Q_ARG(QString, strElementID),
+													Q_ARG(QString, strPhrase),
+													Q_ARG(QString, strAutoCompleter),
 													Q_ARG(int, nCursorPos));
 		Q_ASSERT(bSuccess);
 	}
@@ -940,7 +940,7 @@ void CWebChannelThreadController::resolvePassageReference(CWebChannelObjects *pC
 		bool bSuccess = QMetaObject::invokeMethod(pSearchResults,
 													"resolvePassageReference",
 													Qt::QueuedConnection,
-													Q_ARG(const QString &, strPassageReference));
+													Q_ARG(QString, strPassageReference));
 		Q_ASSERT(bSuccess);
 	}
 }
@@ -955,7 +955,7 @@ void CWebChannelThreadController::gotoIndex(CWebChannelObjects *pChannel, uint32
 													Qt::QueuedConnection,
 													Q_ARG(uint32_t, ndxRel),
 													Q_ARG(int, nMoveMode),
-													Q_ARG(const QString &, strParam));
+													Q_ARG(QString, strParam));
 		Q_ASSERT(bSuccess);
 	}
 }
@@ -968,8 +968,8 @@ void CWebChannelThreadController::gotoChapter(CWebChannelObjects *pChannel, unsi
 		bool bSuccess = QMetaObject::invokeMethod(pSearchResults,
 													"gotoChapter",
 													Qt::QueuedConnection,
-													Q_ARG(unsigned int, nChp),
-													Q_ARG(const QString &, strParam));
+													Q_ARG(uint, nChp),
+													Q_ARG(QString, strParam));
 		Q_ASSERT(bSuccess);
 	}
 }

programs/KJVCanOpener/BibleWordDiffListModel.cpp
@@ -32,7 +32,7 @@ CBibleWordDiffListModel::CBibleWordDiffListModel(CBibleDatabasePtr pBibleDatabas
 {
 	setBibleDatabase(pBibleDatabase);
 
-	connect(TBibleDatabaseList::instance(), SIGNAL(endChangeBibleDatabaseSettings(const QString &, const TBibleDatabaseSettings &, const TBibleDatabaseSettings &, bool)), this, SLOT(en_endChangeBibleDatabaseSettings(const QString &, const TBibleDatabaseSettings &, const TBibleDatabaseSettings &, bool)));
+	connect(TBibleDatabaseList::instance(), SIGNAL(endChangeBibleDatabaseSettings(QString,TBibleDatabaseSettings,TBibleDatabaseSettings,bool)), this, SLOT(en_endChangeBibleDatabaseSettings(QString,TBibleDatabaseSettings,TBibleDatabaseSettings,bool)));
 }
 
 CBibleWordDiffListModel::~CBibleWordDiffListModel()

programs/KJVCanOpener/BrowserWidget.cpp
@@ -98,17 +98,17 @@ CBrowserWidget::CBrowserWidget(CVerseListModel *pSearchResultsListModel, CBibleD
 	connect(CPersistentSettings::instance(), SIGNAL(changedBrowserNavigationPaneMode(BROWSER_NAVIGATION_PANE_MODE_ENUM)), this, SLOT(setBrowserNavigationPaneMode(BROWSER_NAVIGATION_PANE_MODE_ENUM)));
 	connect(CPersistentSettings::instance(), SIGNAL(changedFootnoteRenderingMode(CPhraseNavigator::FootnoteRenderingModeFlags)), this, SIGNAL(rerender()));
 
-	connect(TBibleDatabaseList::instance(), SIGNAL(beginChangeBibleDatabaseSettings(const QString &, const TBibleDatabaseSettings &, const TBibleDatabaseSettings &, bool)), this, SLOT(en_beginChangeBibleDatabaseSettings(const QString &, const TBibleDatabaseSettings &, const TBibleDatabaseSettings &, bool)));
+	connect(TBibleDatabaseList::instance(), SIGNAL(beginChangeBibleDatabaseSettings(QString,TBibleDatabaseSettings,TBibleDatabaseSettings,bool)), this, SLOT(en_beginChangeBibleDatabaseSettings(QString,TBibleDatabaseSettings,TBibleDatabaseSettings,bool)));
 
 // Data Connections:
 	connect(pSearchResultsListModel, SIGNAL(verseListAboutToChange()), this, SLOT(en_SearchResultsVerseListAboutToChange()));
 	connect(pSearchResultsListModel, SIGNAL(verseListChanged()), this, SLOT(en_SearchResultsVerseListChanged()));
 
 // UI Connections:
-	connect(m_pScriptureBrowser, SIGNAL(gotoIndex(const TPhraseTag &)), &m_dlyGotoIndex, SLOT(trigger(const TPhraseTag &)));
-	connect(&m_dlyGotoIndex, SIGNAL(triggered(const TPhraseTag &)), this, SLOT(gotoIndex(const TPhraseTag &)));
-	connect(this, SIGNAL(en_gotoIndex(const TPhraseTag &)), m_pScriptureBrowser, SLOT(en_gotoIndex(const TPhraseTag &)));
-	connect(m_pScriptureBrowser, SIGNAL(sourceChanged(const QUrl &)), this, SLOT(en_sourceChanged(const QUrl &)));
+	connect(m_pScriptureBrowser, SIGNAL(gotoIndex(TPhraseTag)), &m_dlyGotoIndex, SLOT(trigger(TPhraseTag)));
+	connect(&m_dlyGotoIndex, SIGNAL(triggered(TPhraseTag)), this, SLOT(gotoIndex(TPhraseTag)));
+	connect(this, SIGNAL(en_gotoIndex(TPhraseTag)), m_pScriptureBrowser, SLOT(en_gotoIndex(TPhraseTag)));
+	connect(m_pScriptureBrowser, SIGNAL(sourceChanged(QUrl)), this, SLOT(en_sourceChanged(QUrl)));
 	connect(m_pScriptureBrowser, SIGNAL(cursorPositionChanged()), this, SLOT(en_selectionChanged()));
 
 	connect(ui.btnHideNavigation, SIGNAL(clicked()), this, SLOT(en_clickedHideNavigationPane()));
@@ -121,7 +121,7 @@ CBrowserWidget::CBrowserWidget(CVerseListModel *pSearchResultsListModel, CBibleD
 	connect(ui.comboBibleBk, SIGNAL(currentIndexChanged(int)), this, SLOT(delayBibleBkComboIndexChanged(int)));
 	connect(ui.comboBibleChp, SIGNAL(currentIndexChanged(int)), this, SLOT(delayBibleChpComboIndexChanged(int)));
 
-	connect(ui.widgetPassageReference, SIGNAL(passageReferenceChanged(const TPhraseTag &)), this, SLOT(delayPassageReference(const TPhraseTag &)));
+	connect(ui.widgetPassageReference, SIGNAL(passageReferenceChanged(TPhraseTag)), this, SLOT(delayPassageReference(TPhraseTag)));
 	connect(ui.widgetPassageReference, SIGNAL(enterPressed()), this, SLOT(PassageReferenceEnterPressed()));
 
 	connect(&m_dlyBkCombo, SIGNAL(triggered(int)), this, SLOT(BkComboIndexChanged(int)));
@@ -131,7 +131,7 @@ CBrowserWidget::CBrowserWidget(CVerseListModel *pSearchResultsListModel, CBibleD
 	connect(&m_dlyBibleBkCombo, SIGNAL(triggered(int)), this, SLOT(BibleBkComboIndexChanged(int)));
 	connect(&m_dlyBibleChpCombo, SIGNAL(triggered(int)), this, SLOT(BibleChpComboIndexChanged(int)));
 
-	connect(&m_dlyPassageReference, SIGNAL(triggered(const TPhraseTag &)), this, SLOT(PassageReferenceChanged(const TPhraseTag &)));
+	connect(&m_dlyPassageReference, SIGNAL(triggered(TPhraseTag)), this, SLOT(PassageReferenceChanged(TPhraseTag)));
 
 	connect(m_pScriptureBrowser, SIGNAL(activatedScriptureText()), this, SLOT(en_activatedScriptureText()));
 	connect(ui.widgetPassageReference, SIGNAL(activatedPassageReference()), this, SLOT(en_activatedPassageReference()));
@@ -149,24 +149,24 @@ CBrowserWidget::CBrowserWidget(CVerseListModel *pSearchResultsListModel, CBibleD
 	connect(this, SIGNAL(rerender()), m_pScriptureBrowser, SLOT(rerender()));
 
 	// Highlighting colors changing:
-	connect(CPersistentSettings::instance(), SIGNAL(changedColorSearchResults(const QColor &)), this, SLOT(en_SearchResultsColorChanged(const QColor &)));
-	connect(CPersistentSettings::instance(), SIGNAL(changedColorWordsOfJesus(const QColor &)), this, SLOT(en_WordsOfJesusColorChanged(const QColor &)));
+	connect(CPersistentSettings::instance(), SIGNAL(changedColorSearchResults(QColor)), this, SLOT(en_SearchResultsColorChanged(QColor)));
+	connect(CPersistentSettings::instance(), SIGNAL(changedColorWordsOfJesus(QColor)), this, SLOT(en_WordsOfJesusColorChanged(QColor)));
 	connect(CPersistentSettings::instance(), SIGNAL(changedShowExcludedSearchResultsInBrowser(bool)), this, SLOT(en_ShowExcludedSearchResultsChanged(bool)));
 
-	connect(g_pUserNotesDatabase.data(), SIGNAL(highlighterTagsAboutToChange(const CBibleDatabase *, const QString &)), this, SLOT(en_highlighterTagsAboutToChange(const CBibleDatabase *, const QString &)));
-	connect(g_pUserNotesDatabase.data(), SIGNAL(highlighterTagsChanged(const CBibleDatabase *, const QString &)), this, SLOT(en_highlighterTagsChanged(const CBibleDatabase *, const QString &)));
+	connect(g_pUserNotesDatabase.data(), SIGNAL(highlighterTagsAboutToChange(const CBibleDatabase*,QString)), this, SLOT(en_highlighterTagsAboutToChange(const CBibleDatabase*,QString)));
+	connect(g_pUserNotesDatabase.data(), SIGNAL(highlighterTagsChanged(const CBibleDatabase*,QString)), this, SLOT(en_highlighterTagsChanged(const CBibleDatabase*,QString)));
 	connect(g_pUserNotesDatabase.data(), SIGNAL(aboutToChangeHighlighters()), this, SLOT(en_highlightersAboutToChange()));
 	connect(g_pUserNotesDatabase.data(), SIGNAL(changedHighlighters()), this, SLOT(en_highlightersChanged()));
 
 	// User Notes changing:
-	connect(g_pUserNotesDatabase.data(), SIGNAL(addedUserNote(BIBLE_VERSIFICATION_TYPE_ENUM, const CRelIndex &)), this, SLOT(en_userNoteEvent(BIBLE_VERSIFICATION_TYPE_ENUM, const CRelIndex &)));
-	connect(g_pUserNotesDatabase.data(), SIGNAL(changedUserNote(BIBLE_VERSIFICATION_TYPE_ENUM, const CRelIndex &)), this, SLOT(en_userNoteEvent(BIBLE_VERSIFICATION_TYPE_ENUM, const CRelIndex &)));
-	connect(g_pUserNotesDatabase.data(), SIGNAL(removedUserNote(BIBLE_VERSIFICATION_TYPE_ENUM, const CRelIndex &)), this, SLOT(en_userNoteEvent(BIBLE_VERSIFICATION_TYPE_ENUM, const CRelIndex &)));
+	connect(g_pUserNotesDatabase.data(), SIGNAL(addedUserNote(BIBLE_VERSIFICATION_TYPE_ENUM,CRelIndex)), this, SLOT(en_userNoteEvent(BIBLE_VERSIFICATION_TYPE_ENUM,CRelIndex)));
+	connect(g_pUserNotesDatabase.data(), SIGNAL(changedUserNote(BIBLE_VERSIFICATION_TYPE_ENUM,CRelIndex)), this, SLOT(en_userNoteEvent(BIBLE_VERSIFICATION_TYPE_ENUM,CRelIndex)));
+	connect(g_pUserNotesDatabase.data(), SIGNAL(removedUserNote(BIBLE_VERSIFICATION_TYPE_ENUM,CRelIndex)), this, SLOT(en_userNoteEvent(BIBLE_VERSIFICATION_TYPE_ENUM,CRelIndex)));
 	connect(g_pUserNotesDatabase.data(), SIGNAL(changedAllUserNotes()), this, SLOT(en_allUserNotesChanged()));
 
 	// Cross Refs changing:
-	connect(g_pUserNotesDatabase.data(), SIGNAL(addedCrossRef(BIBLE_VERSIFICATION_TYPE_ENUM, const CRelIndex &, const CRelIndex &)), this, SLOT(en_crossRefsEvent(BIBLE_VERSIFICATION_TYPE_ENUM, const CRelIndex &, const CRelIndex &)));
-	connect(g_pUserNotesDatabase.data(), SIGNAL(removedCrossRef(BIBLE_VERSIFICATION_TYPE_ENUM, const CRelIndex &, const CRelIndex &)), this, SLOT(en_crossRefsEvent(BIBLE_VERSIFICATION_TYPE_ENUM, const CRelIndex &, const CRelIndex &)));
+	connect(g_pUserNotesDatabase.data(), SIGNAL(addedCrossRef(BIBLE_VERSIFICATION_TYPE_ENUM,CRelIndex,CRelIndex)), this, SLOT(en_crossRefsEvent(BIBLE_VERSIFICATION_TYPE_ENUM,CRelIndex,CRelIndex)));
+	connect(g_pUserNotesDatabase.data(), SIGNAL(removedCrossRef(BIBLE_VERSIFICATION_TYPE_ENUM,CRelIndex,CRelIndex)), this, SLOT(en_crossRefsEvent(BIBLE_VERSIFICATION_TYPE_ENUM,CRelIndex,CRelIndex)));
 	connect(g_pUserNotesDatabase.data(), SIGNAL(changedAllCrossRefs()), this, SLOT(en_allCrossRefsChanged()));
 }
 

commit 4e7f7066ec51ce4f1c8dc94915889afa45480a68

Fix setBibleAudioURLs signal in webChannelSearchResults.cpp. This was broken in 7d6cb5d790b73af7535649b97e9e83c03a4f08a7.

programs/KJVCanOpener/qwebchannel/webChannelSearchResults.cpp
@@ -738,7 +738,7 @@ CWebChannelSearchResults *CWebChannelThreadController::createWebChannelSearchRes
 		connect(pSearchResults, SIGNAL(resolvedPassageReference(uint32_t,uint32_t)), pChannel, SIGNAL(resolvedPassageReference(uint32_t,uint32_t)));
 
 		connect(pSearchResults, SIGNAL(scriptureBrowserRender(uint,uint32_t,QString,QString)), pChannel, SIGNAL(scriptureBrowserRender(uint,uint32_t,QString,QString)));
-		connect(pSearchResults, SIGNAL(setBibleAudioURLs(QString)), pChannel, SIGNAL(setBibleAudioURLs(QString&)));
+		connect(pSearchResults, SIGNAL(setBibleAudioURLs(QString)), pChannel, SIGNAL(setBibleAudioURLs(QString)));
 	}
 
 	// Initialize after connects in case pSearchResults wants to emit any signals during init: