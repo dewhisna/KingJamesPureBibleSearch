dewhisna/KingJamesPureBibleSearch

commit a110781a0762b6604335649e5522a0ae89693319

Port KJVCanOpener/KJVDataParse to new CXmlReader class. And update all CMakeLists that referenced them as dependencies. This eliminates the need for the Core5Compat library to get the SAX XML parser that Qt deprecated.  This is the last "major" Qt5->Qt6 deprecation.  There are still some API deprecations encountered when DEPRECATION_DISABLE_APIS is enabled, mostly related to QAction/QMenu.

programs/KJVDataParse/main.cpp
@@ -27,6 +27,7 @@
 #include "../KJVCanOpener/Translator.h"
 #include "../KJVCanOpener/CSV.h"
 #include "../KJVCanOpener/BibleLayout.h"
+#include "../KJVCanOpener/XML.h"
 
 #include "xc_KJVDataParse.h"
 
@@ -43,8 +44,6 @@
 #else
 #include <QRegExp>
 #endif
-#include <QXmlAttributes>
-#include <QXmlDefaultHandler>
 #include <QStringList>
 #include <QtGlobal>
 #include <QSettings>
@@ -304,8 +303,8 @@ static QString WordFromWordSet(const TWordListSet &setAltWords)
 // ============================================================================
 // ============================================================================
 
-// Like the QXmlAttributes::index() function, but case-insensitive
-static int findAttribute(const QXmlAttributes &attr, const QString &strName)
+// Like the CXmlAttributes::index() function, but case-insensitive
+static int findAttribute(const CXmlAttributes &attr, const QString &strName)
 {
 	for (int i = 0; i < attr.count(); ++i) {
 		if (attr.localName(i).compare(strName, Qt::CaseInsensitive) == 0) return i;
@@ -313,7 +312,7 @@ static int findAttribute(const QXmlAttributes &attr, const QString &strName)
 	return -1;
 }
 
-static QString stringifyAttributes(const QXmlAttributes &attr)
+static QString stringifyAttributes(const CXmlAttributes &attr)
 {
 	QString strTemp;
 	for (int i=0; i<attr.count(); ++i) {
@@ -323,9 +322,9 @@ static QString stringifyAttributes(const QXmlAttributes &attr)
 	return strTemp;
 }
 
-//static QXmlAttributes attributesFromString(const QString &str)
+//static CXmlAttributes attributesFromString(const QString &str)
 //{
-//	QXmlAttributes attrs;
+//	CXmlAttributes attrs;
 //	QStringList lstPairs = str.split(',');
 //	for (int i=0; i<lstPairs.count(); ++i) {
 //		QStringList lstEntry = lstPairs.at(i).split('=');
@@ -342,7 +341,7 @@ static QString stringifyAttributes(const QXmlAttributes &attr)
 // ============================================================================
 // ============================================================================
 
-class CStrongsImpXmlHandler : public QXmlDefaultHandler
+class CStrongsImpXmlHandler : public CXmlDefaultHandler
 {
 	enum STRONGS_IMP_PARSER_STATE {
 		SIPSE_ENTRYFREE = 0,
@@ -365,15 +364,13 @@ public:
 	virtual ~CStrongsImpXmlHandler()
 	{ }
 
-	virtual bool startElement(const QString &namespaceURI, const QString &localName, const QString &qName, const QXmlAttributes &atts) override;
+	virtual bool startElement(const QString &namespaceURI, const QString &localName, const QString &qName, const CXmlAttributes &atts) override;
 	virtual bool endElement(const QString &namespaceURI, const QString &localName, const QString &qName) override;
 	virtual bool characters(const QString &ch) override;
-	virtual bool error(const QXmlParseException &exception) override;
-	virtual bool fatalError(const QXmlParseException &exception) override;
-	virtual bool warning(const QXmlParseException &exception) override;
+	virtual bool error(const CXmlParseException &exception) override;
 	virtual QString errorString() const override
 	{
-		return (!m_strErrorString.isEmpty() ? m_strErrorString : QXmlDefaultHandler::errorString());
+		return (!m_strErrorString.isEmpty() ? m_strErrorString : CXmlDefaultHandler::errorString());
 	}
 	virtual bool endDocument() override;
 
@@ -431,7 +428,7 @@ void CStrongsImpXmlHandler::endRenderElement()
 	m_lstRenderElementStack.pop_back();
 }
 
-bool CStrongsImpXmlHandler::startElement(const QString &namespaceURI, const QString &localName, const QString &qName, const QXmlAttributes &atts)
+bool CStrongsImpXmlHandler::startElement(const QString &namespaceURI, const QString &localName, const QString &qName, const CXmlAttributes &atts)
 {
 	Q_UNUSED(namespaceURI);
 	Q_UNUSED(qName);
@@ -680,23 +677,13 @@ bool CStrongsImpXmlHandler::characters(const QString &ch)
 	return true;
 }
 
-bool CStrongsImpXmlHandler::error(const QXmlParseException &exception)
+bool CStrongsImpXmlHandler::error(const CXmlParseException &exception)
 {
 	std::cerr << QString("\n\n*** %1\n").arg(exception.message()).toUtf8().data();
-	std::cerr << QString("Line: %1  Column: %2\nPublicID: \"%3\"\nSystemID: \"%4\"\n")
+	std::cerr << QString("Line: %1  Column: %2\nErrorCode: %3\n")
 					.arg(exception.lineNumber()).arg(exception.columnNumber())
-					.arg(exception.publicId()).arg(exception.systemId()).toUtf8().data();
+					.arg(exception.error()).toUtf8().data();
 	return true;
-}
-
-bool CStrongsImpXmlHandler::fatalError(const QXmlParseException &exception)
-{
-	return error(exception);
-}
-
-bool CStrongsImpXmlHandler::warning(const QXmlParseException &exception)
-{
-	return error(exception);
 }
 
 bool CStrongsImpXmlHandler::endDocument()
@@ -707,7 +694,7 @@ bool CStrongsImpXmlHandler::endDocument()
 // ============================================================================
 // ============================================================================
 
-class COSISXmlHandler : public QXmlDefaultHandler
+class COSISXmlHandler : public CXmlDefaultHandler
 {
 public:
 	enum XML_FORMAT_TYPE_ENUM {
@@ -792,15 +779,13 @@ public:
 	QStringList elementNames() const { return m_lstElementNames; }
 	QStringList attrNames() const { return m_lstAttrNames; }
 
-	virtual bool startElement(const QString &namespaceURI, const QString &localName, const QString &qName, const QXmlAttributes &atts) override;
+	virtual bool startElement(const QString &namespaceURI, const QString &localName, const QString &qName, const CXmlAttributes &atts) override;
 	virtual bool endElement(const QString &namespaceURI, const QString &localName, const QString &qName) override;
 	virtual bool characters(const QString &ch) override;
-	virtual bool error(const QXmlParseException &exception) override;
-	virtual bool fatalError(const QXmlParseException &exception) override;
-	virtual bool warning(const QXmlParseException &exception) override;
+	virtual bool error(const CXmlParseException &exception) override;
 	virtual QString errorString() const override
 	{
-		return (!m_strErrorString.isEmpty() ? m_strErrorString : QXmlDefaultHandler::errorString());
+		return (!m_strErrorString.isEmpty() ? m_strErrorString : CXmlDefaultHandler::errorString());
 	}
 	virtual bool endDocument() override;
 
@@ -1014,7 +999,7 @@ const CBookEntry *COSISXmlHandler::addBookToBibleDatabase(int nBk)				// 0-based
 	return &m_pBibleDatabase->m_itrCurrentLayout->m_lstBooks[nBkText];
 }
 
-bool COSISXmlHandler::startElement(const QString &namespaceURI, const QString &localName, const QString &qName, const QXmlAttributes &atts)
+bool COSISXmlHandler::startElement(const QString &namespaceURI, const QString &localName, const QString &qName, const CXmlAttributes &atts)
 {
 	Q_UNUSED(namespaceURI);
 	Q_UNUSED(qName);
@@ -1753,23 +1738,13 @@ bool COSISXmlHandler::characters(const QString &ch)
 	return true;
 }
 
-bool COSISXmlHandler::error(const QXmlParseException &exception)
+bool COSISXmlHandler::error(const CXmlParseException &exception)
 {
 	std::cerr << QString("\n\n*** %1\n").arg(exception.message()).toUtf8().data();
-	std::cerr << QString("Line: %1  Column: %2\nPublicID: \"%3\"\nSystemID: \"%4\"\n")
+	std::cerr << QString("Line: %1  Column: %2\nErrorCode: %3\n")
 					.arg(exception.lineNumber()).arg(exception.columnNumber())
-					.arg(exception.publicId()).arg(exception.systemId()).toUtf8().data();
+					.arg(exception.error()).toUtf8().data();
 	return true;
-}
-
-bool COSISXmlHandler::fatalError(const QXmlParseException &exception)
-{
-	return error(exception);
-}
-
-bool COSISXmlHandler::warning(const QXmlParseException &exception)
-{
-	return error(exception);
 }
 
 bool COSISXmlHandler::endDocument()
@@ -1816,12 +1791,10 @@ bool COSISXmlHandler::endDocument()
 		}
 
 		QBuffer xmlBuffer(&baDataLine);
-		QXmlInputSource xmlInput(&xmlBuffer);
-		QXmlSimpleReader xmlReader;
+		CXmlReader xmlReader(&xmlBuffer);
 
-		xmlReader.setContentHandler(&xmlHandler);
-		xmlReader.setErrorHandler(&xmlHandler);
-		if (xmlReader.parse(xmlInput)) {
+		xmlReader.setXmlHandler(&xmlHandler);
+		if (xmlReader.parse()) {
 			if (m_pBibleDatabase->m_mapStrongsEntries.find(xmlHandler.strongsEntry().strongsMapIndex()) !=
 				m_pBibleDatabase->m_mapStrongsEntries.cend()) {
 				std::cerr << QString("\n*** Duplicate Strongs Map Index: %1\n").arg(strIndexLine).toUtf8().data();
@@ -3644,8 +3617,7 @@ int main(int argc, char *argv[])
 		return ERR_CODE_OSIS_FILE_MISSING;
 	}
 
-	QXmlInputSource xmlInput(&fileOSIS);
-	QXmlSimpleReader xmlReader;
+	CXmlReader xmlReader(&fileOSIS);
 	COSISXmlHandler xmlHandler(bblDescriptor);
 
 	xmlHandler.setNoColophonVerses(bNoColophonVerses);
@@ -3662,11 +3634,10 @@ int main(int argc, char *argv[])
 	xmlHandler.setSegVariant(strSegVariant);
 	xmlHandler.setStrongsImpFilepath(strStrongsImpPath);
 
-	xmlReader.setContentHandler(&xmlHandler);
-	xmlReader.setErrorHandler(&xmlHandler);
+	xmlReader.setXmlHandler(&xmlHandler);
 //	xmlReader.setFeature("http://www.bibletechnologies.net/2003/OSIS/namespace", true);
 
-	if (!xmlReader.parse(xmlInput)) {
+	if (!xmlReader.parse()) {
 		std::cerr << QString("\n\n*** Failed to parse OSIS database \"%1\"\n%2\n").arg(strOSISFilename).arg(xmlHandler.errorString()).toUtf8().data();
 		return ERR_CODE_OSIS_PARSE_FAILED;
 	}

programs/KJVCanOpener/CMakeLists.txt
@@ -157,10 +157,6 @@ endif()
 list(REMOVE_DUPLICATES QT_COMPONENTS)
 
 find_package(QT NAMES Qt6 Qt5 REQUIRED COMPONENTS ${QT_COMPONENTS} ${QT_COMPONENTS_NOLINK} OPTIONAL_COMPONENTS ${QT_OPTIONAL_COMPONENTS})
-if(QT_VERSION_MAJOR EQUAL 6)
-	# Qt6 drops SAX XML Parser which we need.  For now, get it from the Qt5 compatibility shim:
-	list(APPEND QT_COMPONENTS Core5Compat)
-endif()
 find_package(Qt${QT_VERSION_MAJOR} REQUIRED COMPONENTS ${QT_COMPONENTS} ${QT_COMPONENTS_NOLINK} OPTIONAL_COMPONENTS ${QT_OPTIONAL_COMPONENTS})
 set(QT_LINK_LIBS)
 foreach(_component ${QT_COMPONENTS})
@@ -230,6 +226,7 @@ set(KJVCanOpener_SOURCES
 	"VerseListModel.cpp"
 	"VerseListDelegate.cpp"
 	"VerseRichifier.cpp"
+	"XML.cpp"
 )
 
 if(OPTION_BUILD_BIBLE_DATABASE)
@@ -344,6 +341,7 @@ set(KJVCanOpener_HEADERS
 	"VerseListDelegate.h"
 	"VerseRichifier.h"
 	"version.h"
+	"XML.h"
 )
 
 if(OPTION_BUILD_BIBLE_DATABASE)

programs/KJVCanOpener/KJVCanOpener.pro
@@ -35,8 +35,6 @@ greaterThan(QT_MAJOR_VERSION,4) {
 
 QT       *= core gui xml
 
-# Qt6 drops SAX XML Parser which we need.  For now, get it from the Qt5 compatibility shim:
-greaterThan(QT_MAJOR_VERSION,5):QT*=core5compat
 DEFINES *= QT_DEPRECATED_WARNINGS
 
 defined(qtHaveModule, test) {
@@ -373,7 +371,8 @@ SOURCES += \
 	UserNotesDatabase.cpp \
 	VerseListModel.cpp \
 	VerseListDelegate.cpp \
-	VerseRichifier.cpp
+	VerseRichifier.cpp \
+	XML.cpp
 
 buildKJVDatabase:SOURCES += \
 	BuildDB.cpp
@@ -452,7 +451,8 @@ HEADERS += \
 	VerseListModel.h \
 	VerseListDelegate.h \
 	VerseRichifier.h \
-	version.h
+	version.h \
+	XML.h
 
 buildKJVDatabase:HEADERS += \
 	BuildDB.h

programs/KJVCanOpener/UserNotesDatabase.cpp
@@ -310,7 +310,7 @@ bool CUserNotesDatabase::characters(const QString &strChars)
 	return true;
 }
 
-bool CUserNotesDatabase::startElement(const QString &namespaceURI, const QString &localName, const QString &qName, const QXmlAttributes &attr)
+bool CUserNotesDatabase::startElement(const QString &namespaceURI, const QString &localName, const QString &qName, const CXmlAttributes &attr)
 {
 	Q_UNUSED(qName);
 
@@ -657,7 +657,7 @@ bool CUserNotesDatabase::endElement(const QString &namespaceURI, const QString &
 QString CUserNotesDatabase::errorString() const
 {
 	QString strErrorText = m_strLastError;
-	QString strReaderError = QXmlDefaultHandler::errorString();
+	QString strReaderError = CXmlDefaultHandler::errorString();
 	if ((!strErrorText.isEmpty()) && (!strReaderError.isEmpty())) strErrorText += QChar('\n');
 	strErrorText += strReaderError;
 	return strErrorText;
@@ -709,16 +709,12 @@ bool CUserNotesDatabase::load(QIODevice *pIODevice)
 		return false;
 	}
 
-	QXmlInputSource xmlInput(&inUND);
-	QXmlSimpleReader xmlReader;
-
-	xmlReader.setContentHandler(this);
-	xmlReader.setErrorHandler(this);
-	xmlReader.setLexicalHandler(this);
+	CXmlReader xmlReader(&inUND);
+	xmlReader.setXmlHandler(this);
 
 	clearXMLVars();
 
-	if (!xmlReader.parse(xmlInput)) {
+	if (!xmlReader.parse()) {
 		m_strLastError = tr("Failed to read and parse King James User Notes Database File!", "KJNErrors") + QString("\n\n") + errorString();
 		inUND.close();
 		return false;

programs/KJVCanOpener/UserNotesDatabase.h
@@ -26,10 +26,9 @@
 
 #include "dbstruct.h"
 #include "PersistentSettings.h"
+#include "XML.h"
 
 #include <map>
-#include <QXmlAttributes>
-#include <QXmlDefaultHandler>
 #include <QSharedPointer>
 #include <QIODevice>
 #include <QStringList>
@@ -240,7 +239,7 @@ typedef std::map<QString, TCrossReferenceMap> TVersificationCrossRefMap;					// 
 
 // ============================================================================
 
-class CUserNotesDatabase : public QObject, protected QXmlDefaultHandler
+class CUserNotesDatabase : public QObject, protected CXmlDefaultHandler
 {
 	Q_OBJECT
 
@@ -412,7 +411,7 @@ signals:
 protected:
 	// XML Parsing overrides:
 	virtual bool characters(const QString &strChars) override;
-	virtual bool startElement(const QString &namespaceURI, const QString &localName, const QString &qName, const QXmlAttributes &attr) override;
+	virtual bool startElement(const QString &namespaceURI, const QString &localName, const QString &qName, const CXmlAttributes &attr) override;
 	virtual bool endElement(const QString &namespaceURI, const QString &localName, const QString &qName) override;
 	virtual QString errorString() const override;
 	virtual bool startCDATA() override;
@@ -420,7 +419,7 @@ protected:
 
 private:
 	void clearXMLVars();
-	static int findAttribute(const QXmlAttributes &attr, const QString &strName)
+	static int findAttribute(const CXmlAttributes &attr, const QString &strName)
 	{
 		for (int i = 0; i < attr.count(); ++i) {
 			if (attr.localName(i).compare(strName, Qt::CaseInsensitive) == 0) return i;

programs/KJVDataDump/CMakeLists.txt
@@ -67,10 +67,6 @@ set(USE_GEMATRIA ON)
 # -----------------------------------------------------------------------------
 
 find_package(QT NAMES Qt6 Qt5 REQUIRED COMPONENTS ${QT_COMPONENTS} ${QT_COMPONENTS_NOLINK} OPTIONAL_COMPONENTS ${QT_OPTIONAL_COMPONENTS})
-if(QT_VERSION_MAJOR EQUAL 6)
-	# Qt6 drops SAX XML Parser which we need.  For now, get it from the Qt5 compatibility shim:
-	list(APPEND QT_COMPONENTS Core5Compat)
-endif()
 find_package(Qt${QT_VERSION_MAJOR} REQUIRED COMPONENTS ${QT_COMPONENTS} ${QT_COMPONENTS_NOLINK} OPTIONAL_COMPONENTS ${QT_OPTIONAL_COMPONENTS})
 set(QT_LINK_LIBS)
 foreach(_component ${QT_COMPONENTS})
@@ -110,6 +106,7 @@ set(KJVDataDump_SOURCES
 	"../KJVCanOpener/Translator.cpp"
 	"../KJVCanOpener/UserNotesDatabase.cpp"
 	"../KJVCanOpener/VerseRichifier.cpp"
+	"../KJVCanOpener/XML.cpp"
 	# ----
 	"../KJVCanOpener/qwebchannel/webChannelBibleAudio.cpp"
 )
@@ -146,6 +143,7 @@ set(KJVDataDump_HEADERS
 	"../KJVCanOpener/Translator.h"
 	"../KJVCanOpener/UserNotesDatabase.h"
 	"../KJVCanOpener/VerseRichifier.h"
+	"../KJVCanOpener/XML.h"
 	# ----
 	"../KJVCanOpener/qwebchannel/webChannelBibleAudio.h"
 )

programs/KJVDataParse/CMakeLists.txt
@@ -66,10 +66,6 @@ set(CONSOLE ON)
 # -----------------------------------------------------------------------------
 
 find_package(QT NAMES Qt6 Qt5 REQUIRED COMPONENTS ${QT_COMPONENTS} ${QT_COMPONENTS_NOLINK} OPTIONAL_COMPONENTS ${QT_OPTIONAL_COMPONENTS})
-if(QT_VERSION_MAJOR EQUAL 6)
-	# Qt6 drops SAX XML Parser which we need.  For now, get it from the Qt5 compatibility shim:
-	list(APPEND QT_COMPONENTS Core5Compat)
-endif()
 find_package(Qt${QT_VERSION_MAJOR} REQUIRED COMPONENTS ${QT_COMPONENTS} ${QT_COMPONENTS_NOLINK} OPTIONAL_COMPONENTS ${QT_OPTIONAL_COMPONENTS})
 set(QT_LINK_LIBS)
 foreach(_component ${QT_COMPONENTS})
@@ -107,6 +103,7 @@ set(KJVDataParse_SOURCES
 	"../KJVCanOpener/Translator.cpp"
 	"../KJVCanOpener/UserNotesDatabase.cpp"
 	"../KJVCanOpener/VerseRichifier.cpp"
+	"../KJVCanOpener/XML.cpp"
 )
 
 # -------------------------------------
@@ -134,6 +131,7 @@ set(KJVDataParse_HEADERS
 	"../KJVCanOpener/Translator.h"
 	"../KJVCanOpener/UserNotesDatabase.h"
 	"../KJVCanOpener/VerseRichifier.h"
+	"../KJVCanOpener/XML.h"
 )
 
 # -------------------------------------

programs/KJVDictWord/CMakeLists.txt
@@ -66,10 +66,6 @@ set(CONSOLE ON)
 # -----------------------------------------------------------------------------
 
 find_package(QT NAMES Qt6 Qt5 REQUIRED COMPONENTS ${QT_COMPONENTS} ${QT_COMPONENTS_NOLINK} OPTIONAL_COMPONENTS ${QT_OPTIONAL_COMPONENTS})
-if(QT_VERSION_MAJOR EQUAL 6)
-	# Qt6 drops SAX XML Parser which we need.  For now, get it from the Qt5 compatibility shim:
-	list(APPEND QT_COMPONENTS Core5Compat)
-endif()
 find_package(Qt${QT_VERSION_MAJOR} REQUIRED COMPONENTS ${QT_COMPONENTS} ${QT_COMPONENTS_NOLINK} OPTIONAL_COMPONENTS ${QT_OPTIONAL_COMPONENTS})
 set(QT_LINK_LIBS)
 foreach(_component ${QT_COMPONENTS})
@@ -109,6 +105,7 @@ set(KJVDictWord_SOURCES
 	"../KJVCanOpener/Translator.cpp"
 	"../KJVCanOpener/UserNotesDatabase.cpp"
 	"../KJVCanOpener/VerseRichifier.cpp"
+	"../KJVCanOpener/XML.cpp"
 )
 
 # -------------------------------------
@@ -137,6 +134,7 @@ set(KJVDictWord_HEADERS
 	"../KJVCanOpener/Translator.h"
 	"../KJVCanOpener/UserNotesDatabase.h"
 	"../KJVCanOpener/VerseRichifier.h"
+	"../KJVCanOpener/XML.h"
 )
 
 # -------------------------------------

commit f935ee9495803858bc0532659cb14c04386a40a9

Fix Strongs IMP database parser in KJVDataParse. It's necessary to open the xmlBuffer I/O stream before calling xmlReader.parse(). This was apparently broken in a110781a0762b6604335649e5522a0ae89693319 as opening the buffer was originally handled by QXmlInputSource.

programs/KJVDataParse/main.cpp
@@ -1782,6 +1782,7 @@ bool COSISXmlHandler::endDocument()
 		}
 
 		QBuffer xmlBuffer(&baDataLine);
+		xmlBuffer.open(QIODevice::ReadOnly);
 		CXmlReader xmlReader(&xmlBuffer);
 
 		xmlReader.setXmlHandler(&xmlHandler);