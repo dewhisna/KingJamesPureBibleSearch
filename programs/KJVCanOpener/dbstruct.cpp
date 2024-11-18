dewhisna/KingJamesPureBibleSearch

commit abfc9f138d8b84ee4205d0386f973d2d9b2e874d

Fix the PassageReferenceText() and PassageReferenceAbbrText() CRelIndexEx variants outputting of "Colophon" and "Superscription" modifiers.  It was previously making things harder than it needed to be by clearing the word index before calling the base CRelIndex variants.

programs/KJVCanOpener/dbstruct.cpp
@@ -1693,16 +1693,14 @@ QString CBibleDatabase::PassageReferenceText(const CRelIndexEx &nRelIndex, bool 
 	}
 
 	CRelIndex relBase(nRelIndex.index());
-	uint32_t nWord = relBase.word();
-	relBase.setWord(0);
 	QString strRef = PassageReferenceText(relBase, true);
-	if (nWord) {
+	if (relBase.word()) {
 		if (((relBase.chapter() != 0) || ((relBase.chapter() == 0) && !bSuppressWordOnPseudoVerse)) &&
 			((relBase.verse() != 0) || ((relBase.verse() == 0) && !bSuppressWordOnPseudoVerse))) {
 			if (nRelIndex.letter()) {
-				strRef += QString(" [%1.%2]").arg(nWord).arg(nRelIndex.letter());
+				strRef += QString(" [%1.%2]").arg(relBase.word()).arg(nRelIndex.letter());
 			} else {
-				strRef += QString(" [%1]").arg(nWord);
+				strRef += QString(" [%1]").arg(relBase.word());
 			}
 		}
 	}
@@ -1721,16 +1719,14 @@ QString CBibleDatabase::PassageReferenceAbbrText(const CRelIndexEx &nRelIndex, b
 	}
 
 	CRelIndex relBase(nRelIndex.index());
-	uint32_t nWord = relBase.word();
-	relBase.setWord(0);
 	QString strRef = PassageReferenceAbbrText(relBase, true);
-	if (nWord) {
+	if (relBase.word()) {
 		if (((relBase.chapter() != 0) || ((relBase.chapter() == 0) && !bSuppressWordOnPseudoVerse)) &&
 			((relBase.verse() != 0) || ((relBase.verse() == 0) && !bSuppressWordOnPseudoVerse))) {
 			if (nRelIndex.letter()) {
-				strRef += QString(" [%1.%2]").arg(nWord).arg(nRelIndex.letter());
+				strRef += QString(" [%1.%2]").arg(relBase.word()).arg(nRelIndex.letter());
 			} else {
-				strRef += QString(" [%1]").arg(nWord);
+				strRef += QString(" [%1]").arg(relBase.word());
 			}
 		}
 	}

commit 1f2355821db89a5b215eb2cc02b67c58b84a148e

Fix the PassageReferenceText() and PassageReferenceAbbrText() CRelIndexEx variants output broken in abfc9f138d8b84ee4205d0386f973d2d9b2e874d. That previous commit fixed it for the colophons and superscriptions, but broke it for non-pseudoverse references.  This fixes it by setting the word index to zero for non-pseudoverses, but leaving it for pseudoverses.

programs/KJVCanOpener/dbstruct.cpp
@@ -1693,14 +1693,16 @@ QString CBibleDatabase::PassageReferenceText(const CRelIndexEx &nRelIndex, bool 
 	}
 
 	CRelIndex relBase(nRelIndex.index());
+	uint32_t nWord = relBase.word();
+	if (!relBase.isColophon() && !relBase.isSuperscription()) relBase.setWord(0);
 	QString strRef = PassageReferenceText(relBase, true);
-	if (relBase.word()) {
+	if (nWord) {
 		if (((relBase.chapter() != 0) || ((relBase.chapter() == 0) && !bSuppressWordOnPseudoVerse)) &&
 			((relBase.verse() != 0) || ((relBase.verse() == 0) && !bSuppressWordOnPseudoVerse))) {
 			if (nRelIndex.letter()) {
-				strRef += QString(" [%1.%2]").arg(relBase.word()).arg(nRelIndex.letter());
+				strRef += QString(" [%1.%2]").arg(nWord).arg(nRelIndex.letter());
 			} else {
-				strRef += QString(" [%1]").arg(relBase.word());
+				strRef += QString(" [%1]").arg(nWord);
 			}
 		}
 	}
@@ -1719,14 +1721,16 @@ QString CBibleDatabase::PassageReferenceAbbrText(const CRelIndexEx &nRelIndex, b
 	}
 
 	CRelIndex relBase(nRelIndex.index());
+	uint32_t nWord = relBase.word();
+	if (!relBase.isColophon() && !relBase.isSuperscription()) relBase.setWord(0);
 	QString strRef = PassageReferenceAbbrText(relBase, true);
-	if (relBase.word()) {
+	if (nWord) {
 		if (((relBase.chapter() != 0) || ((relBase.chapter() == 0) && !bSuppressWordOnPseudoVerse)) &&
 			((relBase.verse() != 0) || ((relBase.verse() == 0) && !bSuppressWordOnPseudoVerse))) {
 			if (nRelIndex.letter()) {
-				strRef += QString(" [%1.%2]").arg(relBase.word()).arg(nRelIndex.letter());
+				strRef += QString(" [%1.%2]").arg(nWord).arg(nRelIndex.letter());
 			} else {
-				strRef += QString(" [%1]").arg(relBase.word());
+				strRef += QString(" [%1]").arg(nWord);
 			}
 		}
 	}